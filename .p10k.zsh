6aae06c22920b5126d7e187b634177af76d93f61
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-01-17 14:32:03 +0100

    Correct vi-mode icons foreground colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -822,12 +822,15 @@
   ###########[ vi_mode: vi mode (you don't need this if you've enabled prompt_char) ]###########
   # Text and color for normal (a.k.a. command) vi mode.
   typeset -g POWERLEVEL9K_VI_COMMAND_MODE_STRING='󰴗'
+  typeset -g POWERLEVEL9K_VI_MODE_NORMAL_FOREGROUND=0
   typeset -g POWERLEVEL9K_VI_MODE_NORMAL_BACKGROUND='#D00000'
   # Text and color for visual vi mode.
   typeset -g POWERLEVEL9K_VI_VISUAL_MODE_STRING=''
+  typeset -g POWERLEVEL9K_VI_MODE_VISUAL_FOREGROUND=0
   typeset -g POWERLEVEL9K_VI_MODE_VISUAL_BACKGROUND='#D00000'
   # Text and color for overtype (a.k.a. overwrite and replace) vi mode.
   typeset -g POWERLEVEL9K_VI_OVERWRITE_MODE_STRING=''
+  typeset -g POWERLEVEL9K_VI_MODE_OVERWRITE_FOREGROUND=0
   typeset -g POWERLEVEL9K_VI_MODE_OVERWRITE_BACKGROUND='#D00000'
   # Text and color for insert vi mode.
   typeset -g POWERLEVEL9K_VI_INSERT_MODE_STRING='󰏫'
