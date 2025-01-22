8ebd87fc3114b555c8016c0d05ad2a084764305a
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-01-22 17:19:08 +0100

    Clean up nvim-ufo

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -1,18 +1,22 @@
 return {
   'kevinhwang91/nvim-ufo',
-  dependencies = { 'kevinhwang91/promise-async' },
-  opts = function()
-
-    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep:│,foldclose:]]
-    vim.o.foldcolumn = 'auto:2' -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+  dependencies = {
+    'kevinhwang91/promise-async',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  init = function()
+    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep:│,foldclose:'
+    vim.o.foldcolumn = 'auto:9'
+    vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
-    require('ufo').setup({
-      open_fold_hl_timeout = 200,
-      -- provider_selector = function(bufnr, filetype, buftype)
-      --   return {'treesitter', 'indent'}
-      -- end
-    })
   end,
+
+  opts = {
+    open_fold_hl_timeout = 200,
+    provider_selector = function(bufnr, filetype, buftype)
+      return { 'treesitter', 'indent' }
+    end,
+  },
 }
+
