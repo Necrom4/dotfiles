0b5bef8a0421af65731c3eb849686d16dc203f29
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-01-23 11:23:39 +0100

    Update nvim-ufo to be single line

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -3,10 +3,24 @@ return {
   dependencies = {
     'kevinhwang91/promise-async',
     'nvim-treesitter/nvim-treesitter',
+    {
+      "luukvbaal/statuscol.nvim",
+      config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+            relculright = true,
+            segments = {
+              {text = {"%s"}, click = "v:lua.ScSa"},
+              {text = {builtin.foldfunc}, click = "v:lua.ScFa"},
+              {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"}
+            }
+          })
+      end,
+    },
   },
   init = function()
-    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep:│,foldclose:'
-    vim.o.foldcolumn = 'auto:9'
+    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+    vim.o.foldcolumn = '1'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
