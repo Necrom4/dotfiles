35d16ddbec9884bebf01ef270713bca4f2a03e9d
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-01-29 13:48:01 +0100

    Add Snacks scope controls

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -123,7 +123,44 @@ return {
       },
     },
     quickfile = { enabled = true },
-    scope = { enabled = false },
+    scope = {
+      enabled = true,
+      keys = {
+        textobject = {
+          is = {
+            min_size = 2, -- minimum size of the scope
+            edge = false, -- inner scope
+            cursor = false,
+            treesitter = { blocks = { enabled = false } },
+            desc = "inner scope",
+          },
+          as = {
+            cursor = false,
+            min_size = 2, -- minimum size of the scope
+            treesitter = { blocks = { enabled = false } },
+            desc = "full scope",
+          },
+        },
+        jump = {
+          ["[s"] = {
+            min_size = 1, -- allow single line scopes
+            bottom = false,
+            cursor = false,
+            edge = true,
+            treesitter = { blocks = { enabled = false } },
+            desc = "jump to top edge of scope",
+          },
+          ["]s"] = {
+            min_size = 1, -- allow single line scopes
+            bottom = true,
+            cursor = false,
+            edge = true,
+            treesitter = { blocks = { enabled = false } },
+            desc = "jump to bottom edge of scope",
+          },
+        },
+      }
+    },
     scroll = { enabled = false },
     statuscolumn = {
       left = { "sign", "git" }, -- priority of signs on the left (high to low)
