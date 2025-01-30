c3631b0fd2cf9c4597a67746f6d490766f16d5c3
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-01-30 08:24:20 +0100

    Add general settings

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -2,10 +2,29 @@
 
 local opt = vim.opt
 
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.filetype = "on"
+vim.opt.wildmenu = true
+vim.opt.showmatch = true
+vim.opt.ruler = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.formatoptions:remove("cro")
+vim.opt.splitbelow = true
+vim.opt.timeoutlen = 250
+vim.opt.linebreak = true
+vim.opt.termguicolors = true
+
 -- This makes vim act like all other editors, buffers can exist in the background without being in a window.
-
 opt.hidden = true
-
 -- Make macros render faster (lazy draw)
-
 opt.lazyredraw = true
+-- Set global variable g:cwd
+vim.g.cwd = vim.fn.system('~/.scripts/./cwd.sh')
