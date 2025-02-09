c47683501cfb82755725f95f3a7d25f97f58b4c3
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-02-09 12:26:00 +0100

    Add cmp-calc using blink.compat

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,6 +2,11 @@ return {
   'saghen/blink.cmp',
   dependencies = {
     'rafamadriz/friendly-snippets',
+    {
+      'saghen/blink.compat',
+      lazy = true,
+    },
+    'hrsh7th/cmp-calc',
   },
   version = '*',
   event = 'InsertEnter',
@@ -15,38 +20,44 @@ return {
       ['<C-E>'] = { 'cancel' },
       ['<C-D>'] = { 'show', 'show_documentation', 'hide_documentation' },
       ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
-      },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-        accept = {
-          auto_brackets = {
-            enabled = true,
-          },
+    },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono',
+    },
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
         },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 100,
-          window = {
-            border = 'rounded'
-          }
-        },
-        ghost_text = { enabled = true },
-        menu = {
-          draw = {
-            treesitter = { 'lsp' },
-          },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 100,
+        window = {
           border = 'rounded'
+        }
+      },
+      ghost_text = { enabled = true },
+      menu = {
+        draw = {
+          treesitter = { 'lsp' },
         },
-      },
-      signature = { enabled = true },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        border = 'rounded'
       },
     },
-    opts_extend = {
-      "sources.default",
+    signature = { enabled = true },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'calc' },
+      providers = {
+        calc = {
+          name = 'calc',
+          module = 'blink.compat.source',
+        }
+      }
     },
-  }
+  },
+  opts_extend = {
+    "sources.default",
+  },
+}
