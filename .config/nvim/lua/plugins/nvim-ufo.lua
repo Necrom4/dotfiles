a668cad84e02c7c93eba1d920c6f803a11f07024
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-02-12 12:02:29 +0100

    Add folded lines amount

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -5,7 +5,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   init = function()
-    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
     vim.o.foldcolumn = '1'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
@@ -30,6 +30,33 @@ return {
     open_fold_hl_timeout = 200,
     provider_selector = function(bufnr, filetype, buftype)
       return { 'treesitter', 'indent' }
+    end,
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = ('  %d '):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, {chunkText, hlGroup})
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- Ensure padding if truncate returned shorter text
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, {suffix, 'Comment'})
+      return newVirtText
     end,
   },
 }
