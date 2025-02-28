429e973588dfc9a0d8ad152b57ea3355b8c6bb02
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-02-28 15:19:28 +0100

    feat(heirline): add Diagnostics

--- .config/nvim/lua/plugins/heirline.lua
+++ .config/nvim/lua/plugins/heirline.lua
@@ -144,7 +144,7 @@ return {
           local count = self.status_dict.added or 0
           return count > 0 and ("  " .. count)
         end,
-        hl = { fg = colors.green, bg = colors.red_7 },
+        hl = { fg = colors.green },
       },
       {
         provider = function(self)
@@ -157,7 +157,59 @@ return {
           local count = self.status_dict.changed or 0
           return count > 0 and (" 󰏬 " .. count)
         end,
-        hl = { fg = colors.yellow, bg = colors.red_7 },
+        hl = { fg = colors.yellow },
+      },
+    }
+
+    local Diagnostics = {
+
+      condition = conditions.has_diagnostics,
+
+      static = {
+        error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+        warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+        info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+        hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+      },
+
+      init = function(self)
+        self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+        self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+        self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+        self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+      end,
+
+      hl = { bg = colors.red_7 },
+
+      update = { "DiagnosticChanged", "BufEnter" },
+
+      {
+        provider = " "
+      },
+      {
+        provider = function(self)
+          -- 0 is just another output, we can decide to print it or not!
+          return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+        end,
+        hl = { fg = colors.red_1, bold = true },
+      },
+      {
+        provider = function(self)
+          return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+        end,
+        hl = { fg = colors.bright_pink },
+      },
+      {
+        provider = function(self)
+          return self.info > 0 and (self.info_icon .. self.info .. " ")
+        end,
+        hl = { fg = colors.bright_pink },
+      },
+      {
+        provider = function(self)
+          return self.hints > 0 and (self.hint_icon .. self.hints)
+        end,
+        hl = { fg = colors.red_3 },
       },
     }
 
@@ -200,7 +252,7 @@ return {
 
     local FileModifiedWinbar = {
       provider = function()
-        return vim.bo.modified and " " or ""
+        return vim.bo.modified and " " or ""
       end,
     }
 
@@ -265,7 +317,7 @@ return {
     return {
       statusline = { ViMode, FileModifiedVi, LeftSeparator,
         InsertIndicator, { Space, hl = { bg = colors.red_7 }}, FileNameBlock, { Align, hl = { bg = colors.red_7 }},
-        { Space, hl = { bg = colors.red_7 }}, Git, { Space, hl = { bg = colors.red_7 }},
+        { Space, hl = { bg = colors.red_7 }}, Git, { Space, hl = { bg = colors.red_7 }}, Diagnostics, { Space, hl = { bg = colors.red_7 }},
         RightSeparator, { Space, hl = { bg = colors.red_5 }}, CursorPosition, ScrollBar },
       winbar = { WinBars },
       tabline = { TabLine },
