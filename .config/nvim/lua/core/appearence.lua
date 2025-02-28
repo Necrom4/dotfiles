286070c4364b359e208fdfa9b62aacdbb8fd786d
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-02-28 15:19:28 +0100

    feat(heirline): add Diagnostics

--- .config/nvim/lua/core/appearence.lua
+++ .config/nvim/lua/core/appearence.lua
@@ -4,3 +4,8 @@
 vim.opt.showmode = false
 vim.opt.laststatus = 3
 vim.o.showtabline = 1
+
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌶", texthl = "DiagnosticHint" })
