d5b9d7e73cbf33444abc55cac95ce75453e63809
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-03 15:51:58 +0100

    feat(increment-activator): add speeddating plugin

--- .config/nvim/lua/plugins/increment-activator.lua
+++ .config/nvim/lua/plugins/increment-activator.lua
@@ -1,12 +1,24 @@
 return {
-  "nishigori/increment-activator",
-  event = { "BufNewFile", "BufReadPost" },
-  init = function()
-    vim.g.increment_activator_filetype_candidates = {
-      ["_"] = {
-        { "enable", "disable" },
-        { "enabled", "disabled" },
-      },
-    }
-  end,
+  {
+    "nishigori/increment-activator",
+    keys = {
+      { '<c-a>', desc = 'increment' },
+      { '<c-x>', desc = 'decrement' }
+    },
+    init = function()
+      vim.g.increment_activator_filetype_candidates = {
+        ["_"] = {
+          { "enable", "disable" },
+          { "enabled", "disabled" },
+        },
+      }
+    end,
+  },
+  {
+    "tpope/vim-speeddating",
+    keys = {
+      { '<c-a>', desc = 'increment' },
+      { '<c-x>', desc = 'decrement' }
+    },
+  }
 }
