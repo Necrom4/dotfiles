9211b803a53e180a4b3399187ed15d2976362d85
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-04 11:45:45 +0100

    feat(treesitter): add Context trigger + move from dependencies

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,60 +1,73 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-context", -- Show code context
-      event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-      opts = {}
-    }
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    opts = function()
+      local tsc = require("treesitter-context")
+      Snacks.toggle({
+        name = "Treesitter Context",
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map("<leader>c")
+    end,
   },
-  build = function()
-    local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-    ts_update()
-  end,
-  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  opts = {
-    ensure_installed = {
-      'c',
-      'cpp',
-      'bash',
-      'diff',
-      'dockerfile',
-      'git_config',
-      'git_rebase',
-      'gitcommit',
-      'gitignore',
-      'html',
-      'json',
-      'lua',
-      'make',
-      'markdown',
-      'markdown_inline',
-      'python',
-      'php',
-      'ruby',
-      'sql',
-      'ssh_config',
-      'vim',
-      'vimdoc',
-      'yaml',
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        node_decremental = 'v',
-        node_incremental = 'V',
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    opts = {
+      ensure_installed = {
+        'c',
+        'cpp',
+        'bash',
+        'diff',
+        'dockerfile',
+        'git_config',
+        'git_rebase',
+        'gitcommit',
+        'gitignore',
+        'html',
+        'json',
+        'lua',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'php',
+        'ruby',
+        'sql',
+        'ssh_config',
+        'vim',
+        'vimdoc',
+        'yaml',
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_decremental = 'v',
+          node_incremental = 'V',
+        },
       },
     },
-  },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  }
 }
