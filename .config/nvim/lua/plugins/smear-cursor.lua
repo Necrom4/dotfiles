fa4ce10e64acbbadd68369ee9cc69fd5e01c20a6
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-04 10:55:13 +0100

    feat(smear-cursor): add smear-cursor plugin

---
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  opts = {
+    cursor_color = "none",
+    legacy_computing_symbols_support = true,
+    transparentrent_bg_fallback_color = "#330000",
+  },
+}
