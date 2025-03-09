f327c1c9779f306ed4b7eefe55debc3885756283
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-10 00:06:31 +0100

    feat(yazi): add githead plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -15,7 +15,6 @@ require("bookmarks"):setup({
 	},
 })
 
-require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
 require("eza-preview"):setup({
   level = 3,
   follow_symlinks = true,
@@ -23,3 +22,43 @@ require("eza-preview"):setup({
 })
 
 require("git"):setup()
+
+require("githead"):setup({
+  show_branch = true,
+  branch_prefix = "on",
+  branch_color = "#FF0000",
+  branch_symbol = "",
+  branch_borders = "()",
+
+  commit_color = "#CC0000",
+  commit_symbol = "@",
+
+  show_behind_ahead = true,
+  behind_color = "#CC0000",
+  behind_symbol = "",
+  ahead_color = "#CC0000",
+  ahead_symbol = "",
+
+  show_stashes = true,
+  stashes_color = "#CC0000",
+  stashes_symbol = "",
+
+  show_state = true,
+  show_state_prefix = true,
+  state_color = "red",
+  state_symbol = "~",
+
+  show_staged = true,
+  staged_color = "bright yellow",
+  staged_symbol = "",
+
+  show_unstaged = true,
+  unstaged_color = "bright yellow",
+  unstaged_symbol = "",
+
+  show_untracked = true,
+  untracked_color = "blue",
+  untracked_symbol = "?",
+})
+
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
