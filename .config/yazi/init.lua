72b4a4c836edd18f1f6b190f9fe2126e71676902
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-10 14:57:55 +0100

    feat(yazi): use yatline and remove githead

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -38,12 +38,108 @@ THEME.git.updated_sign = "󰇚"
 THEME.git.updated = ui.Style():fg("#00FF00")
 require("git"):setup()
 
-require("githead"):setup({
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
+
+require("yatline"):setup({
+  section_separator = { open = "", close = "" },
+  part_separator = { open = "", close = "" },
+  inverse_separator = { open = "", close = "" },
+
+  style_a = {
+    fg = "#000000",
+    bg_mode = {
+      normal = "#CC0000",
+      select = "#CC0000",
+      un_set = "#CC0000"
+    }
+  },
+  style_b = { bg = "#590000", fg = "#CC0000" },
+  style_c = { bg = "#000000", fg = "#CC0000" },
+
+  permissions_t_fg = "green",
+  permissions_r_fg = "yellow",
+  permissions_w_fg = "red",
+  permissions_x_fg = "cyan",
+  permissions_s_fg = "white",
+
+  tab_width = 20,
+  tab_use_inverse = false,
+
+  selected = { icon = "󰒉", fg = "#CC0000" },
+  copied = { icon = "", fg = "#CC0000" },
+  cut = { icon = "", fg = "#CC0000" },
+
+  total = { icon = "󰮍", fg = "#CC0000" },
+  succ = { icon = "", fg = "#0FF000" },
+  fail = { icon = "", fg = "#0000FF" },
+  found = { icon = "󰮕", fg = "#FFFF00" },
+  processed = { icon = "󰐍", fg = "#00F0F0" },
+
+  show_background = true,
+
+  display_header_line = true,
+  display_status_line = true,
+
+  component_positions = { "header", "tab", "status" },
+
+  header_line = {
+    left = {
+      section_a = {
+        {type = "line", custom = false, name = "tabs", params = {"left"}},
+      },
+      section_b = {
+      },
+      section_c = {
+        {type = "coloreds", custom = false, name = "githead"},
+      }
+    },
+    right = {
+      section_a = {
+      },
+      section_b = {
+      },
+      section_c = {
+      }
+    }
+  },
+
+  status_line = {
+    left = {
+      section_a = {
+        {type = "string", custom = false, name = "tab_mode"},
+      },
+      section_b = {
+        {type = "string", custom = false, name = "hovered_size"},
+      },
+      section_c = {
+        {type = "string", custom = false, name = "hovered_path"},
+        {type = "coloreds", custom = false, name = "count"},
+      }
+    },
+    right = {
+      section_a = {
+        {type = "string", custom = false, name = "cursor_position"},
+      },
+      section_b = {
+        {type = "string", custom = false, name = "cursor_percentage"},
+      },
+      section_c = {
+        {type = "string", custom = false, name = "hovered_file_extension", params = {true}},
+        {type = "coloreds", custom = false, name = "permissions"},
+        {type = "coloreds", custom = false, name = "created_time"},
+      }
+    }
+  },
+})
+
+require("yatline-created-time"):setup()
+
+require("yatline-githead"):setup({
   show_branch = true,
-  branch_prefix = "on",
+  branch_prefix = "",
   branch_color = "#FF0000",
   branch_symbol = "",
-  branch_borders = "()",
+  branch_borders = "",
 
   commit_color = "#CC0000",
   commit_symbol = "@",
@@ -75,5 +171,3 @@ require("githead"):setup({
   untracked_color = "#7F0000",
   untracked_symbol = "?",
 })
-
-require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
