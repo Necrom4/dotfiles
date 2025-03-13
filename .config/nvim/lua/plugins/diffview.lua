5d128bc9e6bd5ecedbff0124cd3c25dc4c2c076d
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-13 11:57:44 +0100

    feat(diffthis): add keymaps and remove load

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,10 @@
 return {
   'sindrets/diffview.nvim',
-  event = { "BufReadPost", "BufWritePre" },
-  opts = {}
+  opts = {},
+  keys = {
+    { "<leader>di", ":DiffviewOpen<CR>", mode = 'n', desc = "Open Current Index Diffview", silent = true },
+    { "<leader>dh", ":DiffviewFileHistory %<CR>", mode = 'n', desc = "Open File History Diffview", silent = true },
+    { "<leader>dh", ":'<,'>DiffviewFileHistory<CR>", mode = 'x', desc = "Open File History Diffview", silent = true },
+    { "<leader>dq", ":DiffviewClose<CR>", desc = "Close Diffview", silent = true },
+  }
 }
