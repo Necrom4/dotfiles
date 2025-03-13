452a5e48332b85024b0021b1b4d3a9d1bbbb30a0
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-13 14:30:11 +0100

    feat(gitsigns): change keymaps according to name logic, remove diff keymaps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -32,22 +32,20 @@ return {
       end)
 
       -- Actions
-      map('n', '<leader>hs', gitsigns.stage_hunk)
-      map('n', '<leader>hr', gitsigns.reset_hunk)
-      map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-      map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-      map('n', '<leader>hS', gitsigns.stage_buffer)
-      map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-      map('n', '<leader>hR', gitsigns.reset_buffer)
-      map('n', '<leader>hp', gitsigns.preview_hunk)
-      map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
-      map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-      map('n', '<leader>hd', gitsigns.diffthis)
-      map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-      map('n', '<leader>td', gitsigns.toggle_deleted)
+      map('n', '<leader>gs', gitsigns.stage_hunk)
+      map('n', '<leader>gr', gitsigns.reset_hunk)
+      map('v', '<leader>gs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('v', '<leader>gr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('n', '<leader>gS', gitsigns.stage_buffer)
+      map('n', '<leader>gu', gitsigns.undo_stage_hunk)
+      map('n', '<leader>gR', gitsigns.reset_buffer)
+      map('n', '<leader>gp', gitsigns.preview_hunk)
+      map('n', '<leader>gP', function() gitsigns.blame_line{full=true} end)
+      map('n', '<leader>gb', gitsigns.toggle_current_line_blame)
+      map('n', '<leader>gd', gitsigns.toggle_deleted)
 
       -- Text object
-      map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      map({'o', 'x'}, 'ig', ':<C-U>Gitsigns select_hunk<CR>')
     end
   }
 }
