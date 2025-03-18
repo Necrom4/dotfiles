6fa478a358b776ed40e5f42146b47057cfed1aeb
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-18 16:23:37 +0100

    fix(tokyonights): add color to WinSeparator highlight

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -6,6 +6,11 @@ return {
     transparent = true,
     styles = {
       floats = "transparent",
-    }
+    },
+    on_highlights = function(hl, c)
+      hl.WinSeparator = {
+        fg = "#C0CAF5",
+      }
+    end,
   },
 }
