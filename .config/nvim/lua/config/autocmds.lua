e5b6c34836ead56a2ffd63f967cfa9c6d375e4be
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-18 17:44:30 +0100

    feat(LazyVim.config): add keymaps and options

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,39 +2,44 @@
 
 -- PERSISTENT UNDO
 vim.opt.undofile = true
-local vimrc_undofile_augroup = vim.api.nvim_create_augroup('vimrc_undofile', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '/tmp/*',
-  group = vimrc_undofile_augroup,
-  command = 'setlocal noundofile',
+local vimrc_undofile_augroup = vim.api.nvim_create_augroup("vimrc_undofile", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "/tmp/*",
+	group = vimrc_undofile_augroup,
+	command = "setlocal noundofile",
 })
 
--- YANK TO NUMBER REGISTERS
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    if vim.v.event.operator == "y" then
-      -- Shift registers "9" to "2" down
-      for i = 9, 2, -1 do
-        vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
-      end
-      -- Store the new yank in register "1" (but don't touch "0")
-      vim.fn.setreg("1", vim.fn.getreg('"'))
-    end
-  end
+-- YANK TO NUMBER REGISTERS AND SYNC SYSTEM CLIPBOARD
+vim.api.nvim_create_autocmd({ "TextYankPost", "FocusGained" }, {
+	callback = function()
+		local clipboard_content = vim.fn.getreg("+") -- Get system clipboard content
+		if clipboard_content and clipboard_content ~= "" then
+			-- Shift registers "9" to "2" down
+			for i = 9, 2, -1 do
+				vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
+			end
+			-- Store system clipboard in register "1" (overwriting it if needed)
+			vim.fn.setreg("1", clipboard_content)
+		end
+	end,
 })
 
 -- TOGGLE COLUMN NUMBER DISPLAY ON MODE SWITCH
-vim.api.nvim_create_autocmd('InsertEnter', {
-  pattern = '*',
-  callback = function() vim.opt_local.relativenumber = false end
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.relativenumber = false
+	end,
 })
 
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = '*',
-  callback = function() vim.opt_local.relativenumber = true end
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.relativenumber = true
+	end,
 })
 
 -- Set embedded templating languages
-vim.cmd [[
+vim.cmd([[
   autocmd BufRead,BufNewFile *.tpl.yaml set filetype=helm
-]]
+]])
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,88 +3,76 @@
 -- SAVE/QUIT
 
 -- Save the current file
-vim.keymap.set('n', '<c-s>', ':w<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-s>", "<cmd>w<cr><esc>", { noremap = true, silent = true })
 -- Force save the current file with force
-vim.keymap.set('n', '<c-s><c-s>', ':w!<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-s><c-s>", "<cmd>w!<cr><esc>", { noremap = true, silent = true })
 -- Save all files
-vim.keymap.set('n', '<c-s><c-a>', ':wa<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-s><c-a>", "<cmd>wa<cr><esc>", { noremap = true, silent = true })
 -- Save and quit
-vim.keymap.set('n', '<c-s><c-q>', ':wq<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-s><c-q>", "<cmd>wq<cr><esc>", { noremap = true, silent = true })
 -- Save all files and quit
-vim.keymap.set('n', '<c-s><c-a><c-q>', ':waq<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-s><c-a><c-q>", "<cmd>waq<cr><esc>", { noremap = true, silent = true })
 -- Quit the current file
-vim.keymap.set('n', '<c-q>', ':q<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-q>", "<cmd>q<cr><esc>", { noremap = true, silent = true })
 -- Force quit the current file
-vim.keymap.set('n', '<c-q><c-q>', ':q!<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-q><c-q>", "<cmd>q!<cr><esc>", { noremap = true, silent = true })
 -- Quit all files
-vim.keymap.set('n', '<c-q><c-a>', ':qa<cr>', { noremap = true, silent = true })
--- Force quit all files
-vim.keymap.set('n', '<c-q><c-a><c-q>', ':qa!<cr>', { noremap = true, silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-q><c-a>", "<cmd>qa<cr><esc>", { noremap = true, silent = true })
+-- Force quit all file
+vim.keymap.set({ "i", "x", "n", "s" }, "<c-q><c-a><c-q>", "<cmd>qa!<cr><esc>", { noremap = true, silent = true })
 
 -- NAVIGATION
-vim.keymap.set({ 'n', 'v' }, '<c-j>', '<c-f>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v' }, '<c-k>', '<c-b>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'MM', 'zz', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'J', 'L', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'K', 'H', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'L', '$', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'v', 'o' }, 'H', '0', { noremap = true, silent = true })
-vim.keymap.set('n', 'gK', 'kgJ', { noremap = true, silent = true })
-vim.keymap.set('n', ';j', '<c-o>', { noremap = true, silent = true })
-vim.keymap.set('n', ';k', '<c-i>', { noremap = true, silent = true })
-vim.keymap.set('n', '<c-s-k>', 'kzz', { noremap = true, silent = true })
-vim.keymap.set('n', '<c-s-j>', 'jzz', { noremap = true, silent = true })
-vim.keymap.set('i', '<c-h>', '<left>', { noremap = true, silent = true })
-vim.keymap.set('i', '<c-j>', '<down>', { noremap = true, silent = true })
-vim.keymap.set('i', '<c-k>', '<up>', { noremap = true, silent = true })
-vim.keymap.set('i', '<c-l>', '<right>', { noremap = true, silent = true })
-vim.keymap.set('c', '<c-h>', '<left>', { noremap = true, silent = true })
-vim.keymap.set('c', '<c-j>', '<down>', { noremap = true, silent = true })
-vim.keymap.set('c', '<c-k>', '<up>', { noremap = true, silent = true })
-vim.keymap.set('c', '<c-l>', '<right>', { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<c-j>", "<c-f>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<c-k>", "<c-b>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "o" }, "MM", "zz", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "o" }, "J", "L", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "o" }, "K", "H", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "o" }, "L", "$", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "o" }, "H", "0", { noremap = true, silent = true })
+vim.keymap.set("n", "gK", "kgJ", { noremap = true, silent = true })
+vim.keymap.set("n", ";j", "<c-o>", { noremap = true, silent = true })
+vim.keymap.set("n", ";k", "<c-i>", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-s-k>", "kzz", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-s-j>", "jzz", { noremap = true, silent = true })
+vim.keymap.set("i", "<c-h>", "<left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<c-j>", "<down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<c-k>", "<up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<c-l>", "<right>", { noremap = true, silent = true })
+vim.keymap.set("c", "<c-h>", "<left>", { noremap = true, silent = true })
+vim.keymap.set("c", "<c-j>", "<down>", { noremap = true, silent = true })
+vim.keymap.set("c", "<c-k>", "<up>", { noremap = true, silent = true })
+vim.keymap.set("c", "<c-l>", "<right>", { noremap = true, silent = true })
+vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader><tab>q", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+vim.keymap.set("n", "]<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
 -- COPY TEXT
 
--- Copy to system clipboard
-vim.keymap.set("v", "<c-c>", '"+y', { noremap = true, silent = true })
 -- Yank filename
-vim.keymap.set('n', 'yp', function()
-  vim.fn.setreg('"', vim.fn.expand('%')) -- Set unnamed register to the file name
-  vim.cmd('normal! p')                   -- Paste the filename
+vim.keymap.set("n", "yp", function()
+	vim.fn.setreg('"', vim.fn.expand("%")) -- Set unnamed register to the file name
+	vim.cmd("normal! p") -- Paste the filename
 end, { noremap = true, silent = true })
 -- Yank full path
-vim.keymap.set('n', 'yP', function()
-  vim.fn.setreg('"', vim.fn.expand('%:p')) -- Set unnamed register to the full path
-  vim.cmd('normal! p')                     -- Paste the path
+vim.keymap.set("n", "yP", function()
+	vim.fn.setreg('"', vim.fn.expand("%:p")) -- Set unnamed register to the full path
+	vim.cmd("normal! p") -- Paste the path
 end, { noremap = true, silent = true })
 -- Yank cmd
-vim.keymap.set('n', 'yc', function()
-  vim.fn.setreg('"', vim.fn.getreg(':')) -- Set unnamed register to the last command
-  vim.cmd('normal! p')                   -- Paste the command
+vim.keymap.set("n", "yc", function()
+	vim.fn.setreg('"', vim.fn.getreg(":")) -- Set unnamed register to the last command
+	vim.cmd("normal! p") -- Paste the command
 end, { noremap = true, silent = true })
 
--- BETTER READABILITY
-vim.keymap.set("n", "<leader>r", function()
-  if vim.wo.wrap then
-    vim.wo.wrap = false
-    vim.cmd("LspStop")
-    vim.cmd("echo 'Better readability ON'")
-  else
-    vim.wo.wrap = true
-    vim.cmd("LspStart")
-    vim.cmd("echo 'Better readability OFF'")
-  end
-end)
-
--- LAZYVIM
-vim.keymap.set('n', '<leader>l', ':Lazy<cr>', { noremap = true, silent = true })
-
 -- LAZYDOCKER
-vim.api.nvim_create_user_command('Lazydocker', function()
-  Snacks.terminal('lazydocker')
+vim.api.nvim_create_user_command("Lazydocker", function()
+	Snacks.terminal("lazydocker")
 end, {})
 
--- OTHER
-
 -- Remap ' to `
-vim.keymap.set('n', "'", '`', { noremap = true })
+vim.keymap.set("n", "'", "`", { noremap = true })
