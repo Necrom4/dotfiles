8266da5dd8738c6be8321f3960dbe37315f4c78b
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-18 22:02:28 +0100

    feat(lualine): bold section headers

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,16 +1,17 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    transparent = true,
-    styles = {
-      floats = "transparent",
-    },
-    on_highlights = function(hl, c)
-      hl.WinSeparator = {
-        fg = "#C0CAF5",
-      }
-    end,
-  },
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		transparent = true,
+		styles = {
+			floats = "transparent",
+		},
+		lualine_bold = true,
+		on_highlights = function(hl, c)
+			hl.WinSeparator = {
+				fg = "#C0CAF5",
+			}
+		end,
+	},
 }
