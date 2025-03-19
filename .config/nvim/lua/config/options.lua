2d7e848783b855373aac6bac35c180665a5b8ad7
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-19 09:31:40 +0100

    feat(options): remove invisible characters and scrolloff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@ local opt = vim.opt
 
 opt.autowrite = false -- Enable auto write
 opt.cursorline = false -- Enable highlighting of the current line
+opt.list = false
+opt.scrolloff = 0
 opt.wrap = false -- Disable line wrap
 
 -- This makes vim act like all other editors, buffers can exist in the background without being in a window.
