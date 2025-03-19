d4204c8394e1759b763205ae1f0d898d4f6a954a
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-19 14:43:38 +0100

    fix(p10k): use default tokyonight colors for path

--- .p10k.zsh
+++ .p10k.zsh
@@ -224,7 +224,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND='#C8D3F5'
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND='#828BB8'
   typeset -g POWERLEVEL9K_DIR_BACKGROUND='#1E2030'
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
@@ -235,7 +235,7 @@
   typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND='#7AA2F7'
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND='#9ECE6A'
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND='#C8D3F5'
   # Display anchor directory segments in bold.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
   # Don't shorten directories that contain any of these files. They are anchors.
