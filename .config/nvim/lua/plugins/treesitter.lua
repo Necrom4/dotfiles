5990e01a761eb2432fda18ca8d4df83c5e071218
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-20 13:13:11 +0100

    chore(treesitter): remove unecessary code from old config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,78 +1,32 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-    opts = function()
-      local tsc = require("treesitter-context")
-      Snacks.toggle({
-        name = "Treesitter Context",
-        get = tsc.enabled,
-        set = function(state)
-          if state then
-            tsc.enable()
-          else
-            tsc.disable()
-          end
-        end,
-      }):map("<leader>c")
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
-    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    opts = {
-      ensure_installed = {
-        'c',
-        'cpp',
-        'css',
-        'scss',
-        'bash',
-        'diff',
-        'dockerfile',
-        'git_config',
-        'git_rebase',
-        'gitcommit',
-        'gitignore',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'make',
-        'markdown',
-        'markdown_inline',
-        'norg',
-        'python',
-        'php',
-        'ruby',
-        'sql',
-        'ssh_config',
-        'toml',
-        'vim',
-        'vimdoc',
-        'yaml',
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_decremental = 'v',
-          node_incremental = 'V',
-        },
-      },
-    },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		opts = function()
+			local tsc = require("treesitter-context")
+			Snacks.toggle({
+				name = "Treesitter Context",
+				get = tsc.enabled,
+				set = function(state)
+					if state then
+						tsc.enable()
+					else
+						tsc.disable()
+					end
+				end,
+			}):map("<leader>uc")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					node_decremental = "v",
+					node_incremental = "V",
+				},
+			},
+		},
+	},
 }
