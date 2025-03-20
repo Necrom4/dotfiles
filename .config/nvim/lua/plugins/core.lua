6276402a8fbbc5a244f5dcbbba697c3b920c93e5
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-20 09:21:45 +0100

    refactor(core): remove useless which-key defaults and reformat file

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,90 +1,18 @@
 return {
-  { "folke/lazy.nvim", version = false },
-  { "LazyVim/LazyVim", version = false },
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        mode = "tabs",
-      },
-    },
-  },
-  {
-    "catppuccin/nvim",
-    enabled = false,
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts_extend = { "spec" },
-    opts = {
-      preset = "helix",
-      defaults = {},
-      spec = {
-        {
-          mode = { "n", "v" },
-          { "<leader><tab>", group = "tabs" },
-          { "<leader>c", group = "code" },
-          { "<leader>d", group = "diff", icon = { icon = " ", color = "yellow" } },
-          { "<a-d>", group = "debug" },
-          { "<leader>dp", group = "profiler" },
-          { "<leader>f", group = "file/find" },
-          { "<leader>g", group = "git" },
-          { "<leader>gh", group = "hunks" },
-          { "<leader>q", group = "quit/session" },
-          { "<leader>/", group = "search" },
-          { "<leader>U", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
-          { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
-          { "[", group = "prev" },
-          { "]", group = "next" },
-          { "g", group = "goto" },
-          { "ys", group = "surround" },
-          { "z", group = "fold" },
-          {
-            "<leader>b",
-            group = "buffer",
-            expand = function()
-              return require("which-key.extras").expand.buf()
-            end,
-          },
-          {
-            "<leader>w",
-            group = "windows",
-            proxy = "<c-w>",
-            expand = function()
-              return require("which-key.extras").expand.win()
-            end,
-          },
-          -- better descriptions
-          { "gx", desc = "Open with system app" },
-        },
-      },
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Keymaps (which-key)",
-      },
-      {
-        "<c-w><space>",
-        function()
-          require("which-key").show({ keys = "<c-w>", loop = true })
-        end,
-        desc = "Window Hydra Mode (which-key)",
-      },
-    },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      if not vim.tbl_isempty(opts.defaults) then
-        LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
-        wk.register(opts.defaults)
-      end
-    end,
-  }
+	{ "folke/lazy.nvim", version = false },
+	{ "LazyVim/LazyVim", version = false },
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				mode = "tabs",
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		enabled = false,
+	},
 	{
 		"folke/noice.nvim",
 		opts = {
@@ -164,4 +92,20 @@ return {
 		},
 	},
 	{
+		"folke/which-key.nvim",
+		opts = {
+			spec = {
+				{
+					mode = { "n", "v" },
+					-- { "<leader>c", group = "code" },
+					{ "<leader>d", group = "diff", icon = { icon = " ", color = "red" } },
+					{ "<leader>q", group = "quit/session" },
+					-- { "<leader>/", group = "search" },
+					{ "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+					-- { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+					-- { "ys", group = "surround" },
+				},
+			},
+		},
+	},
 }
