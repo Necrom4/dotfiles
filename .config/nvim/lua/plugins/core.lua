d55911fbd535bff0976f94793bddce90fa049b77
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-20 09:20:22 +0100

    feat(noice): add keymaps

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -14,14 +14,6 @@ return {
     enabled = false,
   },
   {
-    "folke/noice.nvim",
-    opts = {
-      lsp = {
-        progress = { enabled = false },
-      },
-    },
-  },
-  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts_extend = { "spec" },
@@ -93,4 +85,83 @@ return {
       end
     end,
   }
+	{
+		"folke/noice.nvim",
+		opts = {
+			lsp = {
+				progress = { enabled = false },
+			},
+		},
+		keys = {
+			{ "<leader>N", "", desc = "+noice" },
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
+			},
+			{
+				"<leader>Nl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
+			},
+			{
+				"<leader>Nh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>Na",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<leader>Nd",
+				function()
+					require("noice").cmd("dismiss")
+				end,
+				desc = "Dismiss All",
+			},
+			{
+				"<leader>Nt",
+				function()
+					require("noice").cmd("pick")
+				end,
+				desc = "Noice Picker (Telescope/FzfLua)",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll Forward",
+				mode = { "i", "n", "s" },
+			},
+			{
+				"<c-b>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-b>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll Backward",
+				mode = { "i", "n", "s" },
+			},
+		},
+	},
+	{
 }
