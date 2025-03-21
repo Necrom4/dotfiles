23a78fe47c1ceb8ab4d6ef29d291ecfa1c3e7dfd
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-21 12:02:57 +0100

    feat(tint): make windows darker

--- .config/nvim/lua/plugins/tint.lua
+++ .config/nvim/lua/plugins/tint.lua
@@ -1,7 +1,9 @@
 return {
 	"levouh/tint.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		tint = -75,
+	},
 	keys = {
 		{ "<leader>ut", ":lua require('tint').toggle()<CR>", desc = "Toggle Tint", silent = true },
 	},
