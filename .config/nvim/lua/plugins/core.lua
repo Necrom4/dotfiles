8fef1418539d65c44fc6eff8463c224c39b85109
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-31 15:16:05 +0200

    fix(GrugFar): change keymap

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -56,6 +56,28 @@ return {
 		},
 	},
 	{
+		"MagicDuck/grug-far.nvim",
+		opts = { headerMaxWidth = 80 },
+		cmd = "GrugFar",
+		keys = {
+			{
+				"<leader>R",
+				function()
+					local grug = require("grug-far")
+					local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+					grug.open({
+						transient = true,
+						prefills = {
+							filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+						},
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "Complex Replace",
+			},
+		},
+	},
+	{
 		"folke/noice.nvim",
 		opts = {
 			lsp = {
