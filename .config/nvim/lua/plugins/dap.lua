e1a667e80b44e35842154eb4e777661f5b5e8b7d
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-03-31 15:15:30 +0200

    feat(dap): add bash scripts adapter

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,152 +1,207 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "jay-babu/mason-nvim-dap.nvim",
-    "theHamsta/nvim-dap-virtual-text",
-  },
-  keys = {
-    { '<a-d><a-b>', function() require("dap").toggle_breakpoint() end,  mode = 'n', desc = 'Toggle Breakpoint' },
-    { '<a-d><a-c>', function() require("dap").continue() end,  mode = 'n', desc = 'Continue' },
-    { '<a-d><a-i>', function() require("dap").step_into() end,  mode = 'n', desc = 'Step Into' },
-    { '<a-d><a-o>', function() require("dap").step_over() end,  mode = 'n', desc = 'Step Over' },
-    { '<a-d><a-u>', function() require("dap").step_out() end,  mode = 'n', desc = 'Step Out' },
-    {
-      '<a-d><a-q>',
-      function()
-        require("dap").terminate()
-        require("dapui").close()
-        require("nvim-dap-virtual-text").toggle()
-      end,
-      mode = 'n', desc = 'Terminate'
-    },
-  },
-  config = function()
-    local mason_dap = require("mason-nvim-dap")
-    local dap = require("dap")
-    local ui = require("dapui")
-    local dap_virtual_text = require("nvim-dap-virtual-text")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"jay-babu/mason-nvim-dap.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	keys = {
+		{
+			"<a-d><a-b>",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			mode = "n",
+			desc = "Toggle Breakpoint",
+		},
+		{
+			"<a-d><a-c>",
+			function()
+				require("dap").continue()
+			end,
+			mode = "n",
+			desc = "Continue",
+		},
+		{
+			"<a-d><a-i>",
+			function()
+				require("dap").step_into()
+			end,
+			mode = "n",
+			desc = "Step Into",
+		},
+		{
+			"<a-d><a-o>",
+			function()
+				require("dap").step_over()
+			end,
+			mode = "n",
+			desc = "Step Over",
+		},
+		{
+			"<a-d><a-u>",
+			function()
+				require("dap").step_out()
+			end,
+			mode = "n",
+			desc = "Step Out",
+		},
+		{
+			"<a-d><a-q>",
+			function()
+				require("dap").terminate()
+				require("dapui").close()
+				require("nvim-dap-virtual-text").toggle()
+			end,
+			mode = "n",
+			desc = "Terminate",
+		},
+	},
+	config = function()
+		local mason_dap = require("mason-nvim-dap")
+		local dap = require("dap")
+		local ui = require("dapui")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
 
-    -- Dap Virtual Text
-    dap_virtual_text.setup()
+		-- Dap Virtual Text
+		dap_virtual_text.setup()
 
-    mason_dap.setup({
-      ensure_installed = { "cppdbg", "debugpy", "delve" },
-      automatic_installation = true,
-      handlers = {
-        function(config)
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
-    })
+		mason_dap.setup({
+			ensure_installed = { "bash-debug-adapter", "cppdbg", "debugpy", "delve" },
+			automatic_installation = true,
+			handlers = {
+				function(config)
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
+		})
 
-    -- Configurations
-    dap.configurations = {
-      c = {
-        {
-          name = "Launch",
-          type = "gdb",
-          request = "launch",
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = "${workspaceFolder}",
-          stopAtBeginningOfMainSubprogram = false,
-        },
-        {
-          name = "Select and attach to process",
-          type = "gdb",
-          request = "attach",
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          pid = function()
-            local name = vim.fn.input('Executable name (filter): ')
-            return require("dap.utils").pick_process({ filter = name })
-          end,
-          cwd = '${workspaceFolder}'
-        },
-        {
-          name = 'Attach to gdbserver :1234',
-          type = 'gdb',
-          request = 'attach',
-          target = 'localhost:1234',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}'
-        },
+		-- Configurations
+		dap.configurations = {
+			c = {
+				{
+					name = "Launch",
+					type = "gdb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtBeginningOfMainSubprogram = false,
+				},
+				{
+					name = "Select and attach to process",
+					type = "gdb",
+					request = "attach",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					pid = function()
+						local name = vim.fn.input("Executable name (filter): ")
+						return require("dap.utils").pick_process({ filter = name })
+					end,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					name = "Attach to gdbserver :1234",
+					type = "gdb",
+					request = "attach",
+					target = "localhost:1234",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+				},
+			},
+			python = {
+				{
+					-- The first three options are required by nvim-dap
+					type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+					request = "launch",
+					name = "Launch file",
 
-      },
-      python = {
-        {
-          -- The first three options are required by nvim-dap
-          type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-          request = "launch",
-          name = "Launch file",
+					-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-          -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+					program = "${file}", -- This configuration will launch the current file if used.
+					pythonPath = function()
+						-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+						-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+						-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+						local cwd = vim.fn.getcwd()
+						if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+							return cwd .. "/venv/bin/python"
+						elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+							return cwd .. "/.venv/bin/python"
+						else
+							return "/usr/bin/python3"
+						end
+					end,
+				},
+			},
+			go = {
+				{
+					type = "delve",
+					name = "Debug",
+					request = "launch",
+					program = "${file}",
+				},
+				{
+					type = "delve",
+					name = "Debug test", -- configuration for debugging test files
+					request = "launch",
+					mode = "test",
+					program = "${file}",
+				},
+				-- works with go.mod packages and sub packages
+				{
+					type = "delve",
+					name = "Debug test (go.mod)",
+					request = "launch",
+					mode = "test",
+					program = "./${relativeFileDirname}",
+				},
+			},
+			sh = {
+				{
+					type = "bash",
+					request = "launch",
+					name = "Launch file",
+					showDebugOutput = true,
+					pathBashdb = vim.fn.stdpath("data")
+						.. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+					pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+					trace = true,
+					file = "${file}",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					pathCat = "cat",
+					pathBash = "/bin/bash",
+					pathMkfifo = "mkfifo",
+					pathPkill = "pkill",
+					args = {},
+					env = {},
+					terminalKind = "integrated",
+				},
+			},
+		}
+		-- Dap UI
 
-          program = "${file}", -- This configuration will launch the current file if used.
-          pythonPath = function()
-            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-            local cwd = vim.fn.getcwd()
-            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-              return cwd .. "/venv/bin/python"
-            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-              return cwd .. "/.venv/bin/python"
-            else
-              return "/usr/bin/python3"
-            end
-          end,
-        },
-      },
-      go = {
-        {
-          type = "delve",
-          name = "Debug",
-          request = "launch",
-          program = "${file}",
-        },
-        {
-          type = "delve",
-          name = "Debug test", -- configuration for debugging test files
-          request = "launch",
-          mode = "test",
-          program = "${file}",
-        },
-        -- works with go.mod packages and sub packages
-        {
-          type = "delve",
-          name = "Debug test (go.mod)",
-          request = "launch",
-          mode = "test",
-          program = "./${relativeFileDirname}",
-        },
-      },
-    }
+		ui.setup()
 
-    -- Dap UI
+		vim.fn.sign_define("DapBreakpoint", { text = "" })
 
-    ui.setup()
-
-    vim.fn.sign_define("DapBreakpoint", { text = "" })
-
-    dap.listeners.before.attach.dapui_config = function()
-      ui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      ui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      ui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      ui.close()
-    end
-
-  end
+		dap.listeners.before.attach.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			ui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			ui.close()
+		end
+	end,
 }
