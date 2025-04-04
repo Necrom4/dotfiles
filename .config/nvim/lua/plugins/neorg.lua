1085f6651f57ba99f3ae8307df8a603ca622a38d
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-04-04 11:10:59 +0200

    feat(neorg): add iteration and ToC keymaps

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-neorg/neorg",
 	version = "*",
 	event = { "LazyFile" },
+	ft = { "norg" },
 	opts = {
 		load = {
 			["core.defaults"] = {},
@@ -22,6 +23,20 @@ return {
 			"<leader>N",
 			':lua Snacks.win({ width = 0.4, height = 0.8, position = "float", border = "rounded", wo = { wrap = true, spell = false, number = true, relativenumber = true, foldenable = false, foldcolumn = "0" }, on_win = function(win) vim.cmd("Neorg workspace notes") end, })<CR>',
 			desc = "Open Notes",
+			silent = true,
+		},
+		{
+			"<tab>",
+			"<Plug>(neorg.itero.next-iteration)",
+			mode = "i",
+			ft = "norg",
+			silent = true,
+		},
+		{
+			"gO",
+			"<cmd>Neorg toc<cr>",
+			ft = "norg",
+			desc = "Create a Table of Contents",
 			silent = true,
 		},
 	},
