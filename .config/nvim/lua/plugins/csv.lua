7032f8c989878023c8f193cc2612a8e556fb0df8
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-04-08 09:05:32 +0200

    feat(csv): remove rainbow_csv plugin and add keymap

--- .config/nvim/lua/plugins/csv.lua
+++ .config/nvim/lua/plugins/csv.lua
@@ -1,52 +1,47 @@
 return {
-	{
-		"cameron-wags/rainbow_csv.nvim",
-		config = true,
-		ft = {
-			"csv",
-			"tsv",
-			"csv_semicolon",
-			"csv_whitespace",
-			"csv_pipe",
-			"rfc_csv",
-			"rfc_semicolon",
-		},
-		cmd = {
-			"RainbowDelim",
-			"RainbowDelimSimple",
-			"RainbowDelimQuoted",
-			"RainbowMultiDelim",
+	"hat0uma/csvview.nvim",
+	---@module "csvview"
+	---@type CsvView.Options
+	opts = {
+		parser = { comments = { "#", "//" } },
+		keymaps = {
+			-- Text objects for selecting fields
+			textobject_field_inner = { "if", mode = { "o", "x" } },
+			textobject_field_outer = { "af", mode = { "o", "x" } },
+			-- Excel-like navigation:
+			-- Use <Tab> and <S-Tab> to move horizontally between fields.
+			-- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+			-- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+			jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+			jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+			jump_next_row = { "<Enter>", mode = { "n", "v" } },
+			jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
 		},
 	},
-	{
-		"hat0uma/csvview.nvim",
-		---@module "csvview"
-		---@type CsvView.Options
-		opts = {
-			parser = { comments = { "#", "//" } },
-			keymaps = {
-				-- Text objects for selecting fields
-				textobject_field_inner = { "if", mode = { "o", "x" } },
-				textobject_field_outer = { "af", mode = { "o", "x" } },
-				-- Excel-like navigation:
-				-- Use <Tab> and <S-Tab> to move horizontally between fields.
-				-- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-				-- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-				jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
-				jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
-				jump_next_row = { "<Enter>", mode = { "n", "v" } },
-				jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
-			},
+	ft = {
+		"csv",
+		"tsv",
+		"csv_semicolon",
+		"csv_whitespace",
+		"csv_pipe",
+		"rfc_csv",
+		"rfc_semicolon",
+	},
+	cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+	keys = {
+		{
+			"<leader>ux",
+			function()
+				vim.ui.input({ prompt = "Delimiter: ", default = "," }, function(delimiter)
+					if delimiter and delimiter ~= "" then
+						vim.cmd(
+							string.format("CsvViewToggle display_mode=border header_lnum=1 delimiter=%s", delimiter)
+						)
+					end
+				end)
+			end,
+			desc = "Toggle CSV View",
+			silent = true,
 		},
-		ft = {
-			"csv",
-			"tsv",
-			"csv_semicolon",
-			"csv_whitespace",
-			"csv_pipe",
-			"rfc_csv",
-			"rfc_semicolon",
-		},
-		cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
 	},
 }
