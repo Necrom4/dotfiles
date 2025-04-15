6e6831afc3fc119d28fe223612f9336c88d71c96
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-04-15 11:44:41 +0200

    feat(registers): completely update plugin

--- .config/nvim/lua/plugins/registers.lua
+++ .config/nvim/lua/plugins/registers.lua
@@ -1,16 +1,20 @@
 return {
-  "tversteeg/registers.nvim",
-  cmd = "Registers",
-  config = true,
-  opts = {
-    window = {
-      border = "rounded",
-      transparency = 0,
-    },
-  },
-  keys = {
-    { "\"",    mode = { "n", "v" } },
-    { "<C-R>", mode = "i" }
-  },
-  name = "registers",
+	"tversteeg/registers.nvim",
+	cmd = "Registers",
+	opts = {
+		show = '"*+0123456789abcdefghijklmnopqrstuvwxyz:%#_=-/.',
+		show_empty = false,
+		symbols = {
+			register_type_charwise = "",
+			register_type_linewise = "",
+			register_type_blockwise = "󰹹",
+		},
+		window = {
+			border = "rounded",
+		},
+	},
+	keys = {
+		{ '"', mode = { "n", "v" } },
+		{ "<C-R>", mode = "i" },
+	},
 }
