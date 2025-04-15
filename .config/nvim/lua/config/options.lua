d611d07ad46f6761aaf3a065bef790ed2446f078
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-04-15 10:05:37 +0200

    style(options): sort options and add comments

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,12 +1,8 @@
-local opt = vim.opt
-
-opt.autowrite = false -- Enable auto write
-opt.list = false
-opt.scrolloff = 0
-opt.wrap = false -- Disable line wrap
-
--- This makes vim act like all other editors, buffers can exist in the background without being in a window.
-opt.hidden = true
+vim.opt.autowrite = false -- Enable auto write
+vim.opt.hidden = true -- This makes vim act like all other editors, buffers can exist in the background without being in a window.
+vim.opt.list = false -- Show some invisible characters (tabs...
+vim.opt.scrolloff = 0 -- Lines of context
+vim.opt.wrap = false -- Disable line wrap
 
 vim.diagnostic.config({
 	signs = false,
