d77da36e07af4f80129cd3232380365002b0c9cd
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-04-29 15:51:13 +0200

    feat(snacks.dashboard): keep upating

--- .p10k.zsh
+++ .p10k.zsh
@@ -98,10 +98,12 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
     # vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
+    wifi                  # wifi speed
+    ip                    # ip address and bandwidth usage for a specified network interface
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
-    ram                   # free RAM
+    # ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
