92b741d99aa789db900d3561f5f76f6a969bcd98
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-05-14 11:19:55 +0200

    feat(windows): add separator

--- .config/nvim/lua/plugins/windows.lua
+++ .config/nvim/lua/plugins/windows.lua
@@ -16,4 +16,16 @@ return {
 			{ "<c-w><enter>", ":Win<CR>", desc = "Open Win Manager", silent = true },
 		},
 	},
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		opts = {
+			hi = {
+				bg = "none",
+				fg = "#86E1FC",
+			},
+			symbols = { "─", "│", "┌", "┐", "└", "┘" },
+			only_line_seq = false,
+		},
+		event = { "WinLeave" },
+	},
 }
