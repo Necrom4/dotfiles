dec4bbd89641c52159ef0140ee91a87edb598fb9
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-06-05 15:51:47 +0200

    feat(neovide): working copy-paste ctrl commands on Windows and Mac

--- .config/nvim/lua/core/neovide.lua
+++ .config/nvim/lua/core/neovide.lua
@@ -14,9 +14,31 @@ if vim.g.neovide then
 	vim.opt.linespace = 1
 
 	-- Copy paste
-	vim.g.neovide_input_use_logo = true
-	vim.keymap.set("n", "<D-v>", '"+p', { noremap = true, silent = true })
-	vim.keymap.set("i", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-	vim.keymap.set("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-	vim.keymap.set("v", "<D-c>", '"+y<CR>', { noremap = true, silent = true })
+	local c_c = "<C-c>"
+	local c_v = "<C-v>"
+
+	if system_type == "darwin" then
+		vim.g.neovide_input_use_logo = true
+		c_c = "<D-c>"
+		c_v = "<D-v>"
+	end
+
+	local function paste_in_prompt()
+		local keys = vim.fn.getreg("+")
+		keys = vim.fn.escape(keys, [["\]])
+		local termcodes = vim.api.nvim_replace_termcodes(keys, true, true, true)
+		vim.api.nvim_feedkeys(termcodes, "c", false)
+	end
+
+	vim.keymap.set("n", c_v, '"+p', { noremap = true, silent = true })
+	vim.keymap.set("i", c_v, function()
+		vim.opt.paste = true
+		vim.api.nvim_feedkeys(vim.fn.getreg("+"), "n", false)
+		vim.defer_fn(function()
+			vim.opt.paste = false
+		end, 0)
+	end, { noremap = true, silent = true })
+	vim.keymap.set("c", c_v, paste_in_prompt, { noremap = true, silent = true })
+	vim.keymap.set("t", c_v, paste_in_prompt, { noremap = true, silent = true })
+	vim.keymap.set("v", c_c, '"+y', { noremap = true, silent = true })
 end
