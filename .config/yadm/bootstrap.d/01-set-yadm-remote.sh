af37eaaa6eded54594c89ad5bb8dd8d57fc05167
Diogo Pombeiro <dpombeiro@centrepatronal.ch>
2025-06-05 13:44:21 +0200

    fix(yadm): use https for push if ssh not available
    Not working in WSL

--- .config/yadm/bootstrap.d/01-set-yadm-remote.sh
+++ .config/yadm/bootstrap.d/01-set-yadm-remote.sh
@@ -2,4 +2,12 @@
 
 echo "Updating the yadm repo origin URL"
 yadm remote set-url origin "https://github.com/Necrom4/dotfiles.git"
-yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
+
+ssh -o ConnectTimeout=3 -T git@github.com >/dev/null 2>&1
+ssh_status=$?
+
+if [ "$ssh_status" -eq 1 ] || [ "$ssh_status" -eq 0 ]; then
+  yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
+else
+  yadm remote set-url --push origin "https://github.com/Necrom4/dotfiles.git"
+fi
