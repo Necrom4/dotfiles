87f86ec99e843a24f4fd0d4871bbbbdd42de2f36
Diogo Pombeiro <diogopombeiro@Diogos-MacBook-Pro.local>
2025-06-07 18:34:56 +0200

    feat(render-markdown): add header and checkbox icons

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,27 @@
 return {
 	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			checkbox = {
+				enabled = true,
+				right_pad = 1,
+				unchecked = {
+					icon = "    󰄱",
+					highlight = "RenderMarkdownUnchecked",
+					scope_highlight = nil,
+				},
+				checked = {
+					icon = "    󰡖",
+					highlight = "RenderMarkdownChecked",
+					scope_highlight = nil,
+				},
+			},
+			heading = {
+				icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+			},
+		},
+	},
+	{
 		"iamcco/markdown-preview.nvim", -- markdown preview plugin for (neo)vim
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = "markdown",
