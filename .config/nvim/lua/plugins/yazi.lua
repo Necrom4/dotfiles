7e51bb3babf7150053e9245219cc7fb161ad24b2
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-08-23 01:06:49 +0200

    chore(neovim.yazi): reduce window size and decrease opacity

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,5 +1,9 @@
 return {
 	"mikavilpas/yazi.nvim",
+	opts = {
+		floating_window_scaling_factor = 0.7,
+		yazi_floating_window_winblend = 20,
+	},
 	keys = {
 		{ "<leader>xf", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Floating" },
 	},
