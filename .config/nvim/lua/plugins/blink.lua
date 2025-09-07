8ad2474ca15925e9d4f1403cef371624514e0a13
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-07 09:58:56 +0200

    feat(neovim.blink_cmp): add env, spell, words and yanky

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,12 +6,16 @@ return {
 			"saghen/blink.compat",
 			lazy = true,
 		},
-		"mikavilpas/blink-ripgrep.nvim",
 		"hrsh7th/cmp-calc",
-		"folke/lazydev.nvim",
 		"moyiz/blink-emoji.nvim",
-		"MahanRahmati/blink-nerdfont.nvim",
+		"bydlw98/blink-cmp-env",
 		"Kaiser-Yang/blink-cmp-git",
+		"folke/lazydev.nvim",
+		"MahanRahmati/blink-nerdfont.nvim",
+		"mikavilpas/blink-ripgrep.nvim",
+		"ribru17/blink-cmp-spell",
+		"archie-judd/blink-cmp-words",
+		"marcoSven/blink-cmp-yanky",
 	},
 	version = "*",
 	event = { "InsertEnter", "CmdlineEnter" },
@@ -69,7 +73,22 @@ return {
 		},
 		signature = { enabled = true },
 		sources = {
-			default = { "buffer", "calc", "emoji", "git", "lazydev", "lsp", "nerdfont", "path", "ripgrep", "snippets" },
+			default = {
+				"buffer",
+				"calc",
+				"emoji",
+				"env",
+				"git",
+				"lazydev",
+				"lsp",
+				"nerdfont",
+				"path",
+				"ripgrep",
+				"snippets",
+				"spell",
+				"thesaurus",
+				"yank",
+			},
 			providers = {
 				calc = {
 					name = "calc",
@@ -80,6 +99,15 @@ return {
 					module = "blink-emoji",
 					score_offset = 15, -- Tune by preference
 					opts = { insert = true }, -- Insert emoji (default) or complete its name
+				},
+				env = {
+					name = "Env",
+					module = "blink-cmp-env",
+					opts = {
+						item_kind = require("blink.cmp.types").CompletionItemKind.Variable,
+						show_braces = false,
+						show_documentation_window = true,
+					},
 				},
 				git = {
 					module = "blink-cmp-git",
@@ -100,6 +128,59 @@ return {
 				ripgrep = {
 					name = "Ripgrep",
 					module = "blink-ripgrep",
+				},
+				spell = {
+					name = "Spell",
+					module = "blink-cmp-spell",
+					opts = {
+						enable_in_context = function()
+							local curpos = vim.api.nvim_win_get_cursor(0)
+							local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
+							local in_spell_capture = false
+							for _, cap in ipairs(captures) do
+								if cap.capture == "spell" then
+									in_spell_capture = true
+								elseif cap.capture == "nospell" then
+									return false
+								end
+							end
+							return in_spell_capture
+						end,
+					},
+				},
+				thesaurus = {
+					name = "blink-cmp-words",
+					module = "blink-cmp-words.thesaurus",
+					-- All available options
+					opts = {
+						-- A score offset applied to returned items.
+						-- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+						score_offset = 0,
+
+						-- Default pointers define the lexical relations listed under each definition,
+						-- see Pointer Symbols below.
+						-- Default is as below ("antonyms", "similar to" and "also see").
+						definition_pointers = { "!", "&", "^" },
+
+						-- The pointers that are considered similar words when using the thesaurus,
+						-- see Pointer Symbols below.
+						-- Default is as below ("similar to", "also see" }
+						similarity_pointers = { "&", "^" },
+
+						-- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
+						-- 2 is similar words of similar words, etc. Increasing this may slow results.
+						similarity_depth = 2,
+					},
+				},
+				yank = {
+					name = "yank",
+					module = "blink-yanky",
+					opts = {
+						minLength = 5,
+						onlyCurrentFiletype = true,
+						trigger_characters = { '"' },
+						kind_icon = "󰅍",
+					},
 				},
 			},
 		},
