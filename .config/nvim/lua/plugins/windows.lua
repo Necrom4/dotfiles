a3d5b887a265728b31e69a12c71c479eaf31fd72
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-10 11:35:57 +0200

    feat(neovim): add tiling manager and windows picker for navigation + mixing of both for swapping windows

--- .config/nvim/lua/plugins/windows.lua
+++ .config/nvim/lua/plugins/windows.lua
@@ -12,9 +12,43 @@ return {
 		},
 	},
 	{
-		"dstein64/vim-win",
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		version = "2.*",
+		opts = {
+			hint = "floating-big-letter",
+			selection_chars = "JHKLFDSA",
+			show_prompt = false,
+			filter_rules = {
+				include_current_win = false,
+				bo = {
+					filetype = { "NvimTree", "neo-tree", "notify" },
+				},
+			},
+		},
 		keys = {
-			{ "<c-w><enter>", ":Win<CR>", desc = "Open Win Manager", silent = true },
+			{
+				"<C-w>n",
+				function()
+					local picked_window_id = require("window-picker").pick_window()
+					if picked_window_id then
+						vim.api.nvim_set_current_win(picked_window_id)
+					end
+				end,
+				desc = "Navigate",
+			},
+		},
+	},
+	{
+		"sindrets/winshift.nvim",
+		opts = {
+			window_picker = function()
+				return require("window-picker").pick_window()
+			end,
+		},
+		keys = {
+			{ "<c-w><cr>", "<cmd>WinShift<cr>", desc = "Tiling manager", silent = true },
+			{ "<c-w>S", "<cmd>WinShift swap<cr>", desc = "Swap", silent = true },
 		},
 	},
 }
