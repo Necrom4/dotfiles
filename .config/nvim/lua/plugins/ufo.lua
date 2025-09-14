843c43d5df426fbdecb68a24b79ee8cad05ed2e5
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-14 20:58:10 +0200

    feat(neovim.ufo): change style to include ending of fold
    From @nickkadutskyi on GitHub

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,62 +1,108 @@
+local Fold = require("core.fold")
+
+local ftMap = {
+	vim = "indent",
+	python = { "treesitter", "indent" },
+	lua = { "treesitter", "indent" },
+	javascript = { "treesitter", "indent" },
+	typescript = { "treesitter", "indent" },
+	javascriptreact = { "treesitter", "indent" },
+	typescriptreact = { "treesitter", "indent" },
+	vue = { "indent", "treesitter" },
+	json = { "treesitter", "indent" },
+	jsonc = { "treesitter", "indent" },
+	yaml = { "treesitter", "indent" },
+	toml = { "treesitter", "indent" },
+	rust = { "lsp", "treesitter" },
+	go = { "lsp", "treesitter" },
+	php = { "lsp", "treesitter" },
+	ruby = { "treesitter", "indent" },
+	c = { "lsp", "treesitter" },
+	cpp = { "lsp", "treesitter" },
+	java = { "lsp", "treesitter" },
+	css = { "treesitter", "indent" },
+	scss = { "treesitter", "indent" },
+	html = { "treesitter", "indent" },
+	xml = { "treesitter", "indent" },
+	markdown = { "treesitter", "indent" },
+	sh = { "treesitter", "indent" },
+	zsh = { "treesitter", "indent" },
+	fish = { "treesitter", "indent" },
+	git = "",
+	gitcommit = "",
+	fff_list = "",
+	fff_preview = "",
+	fff_input = "",
+	help = "indent",
+	text = "indent",
+}
+
 return {
-  'kevinhwang91/nvim-ufo',
-  dependencies = {
-    'kevinhwang91/promise-async',
-    'nvim-treesitter/nvim-treesitter',
-  },
-  init = function()
-    vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
-    vim.o.foldcolumn = '1'
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-  end,
-  keys = {
-    { 'zR', function() require('ufo').openAllFolds() end,  mode = { 'n', 'v' }, desc = 'Open All Folds', },
-    { 'zM', function() require('ufo').closeAllFolds() end, mode = { 'n', 'v' }, desc = 'Close All Folds', },
-    {
-      'zp',
-      function()
-        local winid = require('ufo').peekFoldedLinesUnderCursor()
-        if not winid then
-          vim.lsp.buf.hover()
-        end
-      end,
-      desc = 'Peek folded lines',
-      mode = { 'n', 'v' },
-    },
-  },
-  opts = {
-    open_fold_hl_timeout = 200,
-    provider_selector = function(bufnr, filetype, buftype)
-      return { 'treesitter', 'indent' }
-    end,
-    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-      local newVirtText = {}
-      local suffix = ('  %d '):format(endLnum - lnum)
-      local sufWidth = vim.fn.strdisplaywidth(suffix)
-      local targetWidth = width - sufWidth
-      local curWidth = 0
-      for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-          table.insert(newVirtText, chunk)
-        else
-          chunkText = truncate(chunkText, targetWidth - curWidth)
-          local hlGroup = chunk[2]
-          table.insert(newVirtText, {chunkText, hlGroup})
-          chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          -- Ensure padding if truncate returned shorter text
-          if curWidth + chunkWidth < targetWidth then
-            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-          end
-          break
-        end
-        curWidth = curWidth + chunkWidth
-      end
-      table.insert(newVirtText, {suffix, 'FoldedLinesAmount'})
-      return newVirtText
-    end,
-  },
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	init = function()
+		vim.o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldclose:"
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+	end,
+	keys = {
+		{
+			"zR",
+			function()
+				require("ufo").openAllFolds()
+			end,
+			mode = { "n", "v" },
+			desc = "Open All Folds",
+		},
+		{
+			"zM",
+			function()
+				require("ufo").closeAllFolds()
+			end,
+			mode = { "n", "v" },
+			desc = "Close All Folds",
+		},
+		{
+			"zp",
+			function()
+				local winid = require("ufo").peekFoldedLinesUnderCursor()
+				if not winid then
+					vim.lsp.buf.hover()
+				end
+			end,
+			desc = "Peek folded lines",
+			mode = { "n", "v" },
+		},
+	},
+	opts = {
+		open_fold_hl_timeout = 150,
+		enable_get_fold_virt_text = true,
+		fold_virt_text_handler = Fold.ufo_virt_text_handler_enhanced,
+		close_fold_kinds_for_ft = {
+			default = { "imports", "comment" },
+			json = { "array" },
+			jsonc = { "array" },
+			c = { "comment", "region" },
+			cpp = { "comment", "region" },
+			java = { "comment", "imports" },
+			javascript = { "comment", "imports" },
+			typescript = { "comment", "imports" },
+			vue = { "imports" },
+			python = { "comment", "imports" },
+			go = { "comment", "imports" },
+			rust = { "comment", "imports" },
+			php = { "imports" },
+		},
+		provider_selector = function(bufnr, filetype, buftype)
+			if buftype ~= "" then
+				return nil
+			end
+			return ftMap[filetype] or Fold.ufo_provider_selector
+		end,
+	},
 }
