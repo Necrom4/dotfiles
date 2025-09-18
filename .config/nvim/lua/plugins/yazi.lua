c92a18e71297692e2997f4f6a7459ffc1fdf20e6
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-18 15:33:06 +0200

    feat(nvim.yazi): enable path persistence

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -5,6 +5,6 @@ return {
 		yazi_floating_window_winblend = 20,
 	},
 	keys = {
-		{ "<leader>xf", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Floating" },
+		{ "<leader>xf", mode = { "n", "v" }, "<cmd>Yazi toggle<cr>", desc = "Floating" },
 	},
 }
