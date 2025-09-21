41448ab9d59ab3e7dac8df3f5cc5d8435da0d6b2
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-21 23:26:51 +0200

    feat(nvim.colorizer): toggle keymap affects all buffers

--- .config/nvim/lua/plugins/mini-hipatterns.lua
+++ .config/nvim/lua/plugins/mini-hipatterns.lua
@@ -2,32 +2,29 @@ return {
 	"nvim-mini/mini.hipatterns",
 	opts = function(_, opts)
 		local hipatterns = require("mini.hipatterns")
-
-		local function buf_enabled(bufnr)
-			bufnr = bufnr or vim.api.nvim_get_current_buf()
-			for _, b in ipairs(hipatterns.get_enabled_buffers()) do
-				if b == bufnr then
-					return true
-				end
-			end
-			return false
-		end
+		local api = vim.api
 
 		Snacks.toggle({
 			id = "mini-hipatterns",
-			name = "Color",
+			name = "Colorizer",
 			get = function()
-				return buf_enabled()
+				return not vim.g.minihipatterns_disable
 			end,
 			set = function(state)
-				if buf_enabled() ~= state then
-					hipatterns.toggle(0)
-					if state then
-						pcall(hipatterns.update, 0)
+				vim.g.minihipatterns_disable = not state
+				for _, buf in ipairs(api.nvim_list_bufs()) do
+					if api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "" then
+						if state then
+							hipatterns.enable(buf)
+							pcall(hipatterns.update, buf)
+						else
+							hipatterns.disable(buf)
+						end
 					end
 				end
 			end,
 		}):map("<leader>uC")
+
 		return opts
 	end,
 }
