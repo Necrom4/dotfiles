e37548fb5ce054d2d87396cb35347839b3c0e369
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-21 12:24:31 +0200

    fix(nvim.lspconfig): use tbl_deep_extend

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,24 +1,22 @@
 return {
 	"neovim/nvim-lspconfig",
-	opts = function()
+	opts = function(_, opts)
 		local keys = require("lazyvim.plugins.lsp.keymaps").get()
 		-- disable a keymap
 		keys[#keys + 1] = { "K", false }
 		keys[#keys + 1] = { "<leader>ss", false }
 
-		local ret = {
-			diagnostics = {
-				virtual_text = {
-					prefix = "",
-				},
+		opts.diagnostics = vim.tbl_deep_extend("force", opts.diagnostics, {
+			virtual_text = {
+				prefix = "",
 			},
-			inlay_hints = {},
-			codelens = {
-				enabled = false,
-			},
-			servers = {},
-			setup = {},
-		}
-		return ret
+			signs = false,
+		})
+		-- opts.inlay_hints = vim.tbl_deep_extend("force", opts.inlay_hints, {
+		-- 	enabled = false,
+		-- })
+		-- opts.codelens = vim.tbl_deep_extend("force", opts.codelens, {
+		-- 	enabled = false,
+		-- })
 	end,
 }
