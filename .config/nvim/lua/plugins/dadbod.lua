d9b59fd030e834d2f07f7e63a67380353eded717
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-24 20:16:49 +0200

    chore(nvim.dadbod): use better icons

---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,18 @@
+vim.g.db_ui_use_nerd_fonts = 1
+
+vim.g.db_ui_icons = {
+	expanded = {
+		buffers = "▾ ﬘",
+		saved_queries = "▾ 󱔗",
+	},
+	collapsed = {
+		buffers = "▸ ﬘",
+		saved_queries = "▸ 󱔗",
+	},
+	saved_query = "",
+	new_query = "󱇧",
+	connection_ok = "",
+	connection_error = "",
+}
+
+return {}
