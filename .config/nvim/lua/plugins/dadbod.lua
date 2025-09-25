c7c98ae732a71139ea1198defec7d311b49196b2
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-25 17:30:31 +0200

    feat(nvim.dbee): configure options

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -21,12 +21,64 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	build = function()
-		-- Install tries to automatically detect the install method.
-		-- if it fails, try calling it with one of these parameters:
-		--    "curl", "wget", "bitsadmin", "go"
 		require("dbee").install()
 	end,
 	config = function()
-		require("dbee").setup(--[[optional config]])
+		require("dbee").setup({
+			drawer = {
+				disable_help = true,
+				mappings = {
+					{ key = "R", mode = "n", action = "refresh" },
+					{ key = "z", mode = "n", action = "toggle" },
+				},
+				candies = {
+					note = {
+						icon = "",
+						icon_highlight = "Character",
+						text_highlight = "",
+					},
+					schema = {
+						icon = "פּ",
+						icon_highlight = "Removed",
+						text_highlight = "",
+					},
+					help = {
+						icon = "󰘥",
+						icon_highlight = "Title",
+						text_highlight = "Title",
+					},
+					source = {
+						icon = "",
+						icon_highlight = "MoreMsg",
+						text_highlight = "MoreMsg",
+					},
+				},
+			},
+			result = {
+				mappings = {
+					{ key = "K", mode = "", action = "page_last" },
+					{ key = "J", mode = "", action = "page_first" },
+				},
+			},
+			editor = {
+				mappings = {
+					{ key = "<cr>", mode = "v", action = "run_selection" },
+					{ key = "<cr>", mode = "n", action = "run_file" },
+					{ key = "<cr>", mode = "n", action = "run_under_cursor" },
+				},
+			},
+		})
 	end,
+	cmd = "Dbee",
+	keys = {
+		{
+			"D",
+			function()
+				require("dbee").toggle()
+			end,
+			mode = { "n" },
+			silent = true,
+			desc = "Open DBee",
+		},
+	},
 }
