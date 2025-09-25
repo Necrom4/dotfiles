6aff146253ef0274a4608323b48021e419eb9ad9
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-25 16:52:25 +0200

    feat(nvim): add DBee

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -15,4 +15,18 @@ vim.g.db_ui_icons = {
 	connection_error = "",
 }
 
-return {}
+return {
+	"kndndrj/nvim-dbee",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	build = function()
+		-- Install tries to automatically detect the install method.
+		-- if it fails, try calling it with one of these parameters:
+		--    "curl", "wget", "bitsadmin", "go"
+		require("dbee").install()
+	end,
+	config = function()
+		require("dbee").setup(--[[optional config]])
+	end,
+}
