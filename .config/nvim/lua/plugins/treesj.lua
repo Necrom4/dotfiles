dceba4cf7082b38ad564f8d719cad5e7a74ff163
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-09-26 15:42:37 +0200

    wip(nvim.sql): add treesj config for arrays
    In joined mode, there's extra whitespaces at the beginning and end of
    the brackets
    In split mode, the first bracket doesn't go back to the original
    position

--- .config/nvim/lua/plugins/treesj.lua
+++ .config/nvim/lua/plugins/treesj.lua
@@ -6,5 +6,23 @@ return {
 	opts = {
 		use_default_keymaps = false,
 		max_join_length = 1000,
+		langs = {
+			sql = {
+				array = {
+					both = {
+						omit = { "array", "[", "]" },
+					},
+					split = {
+						separator = ",",
+						inner_indent = "normal",
+						last_indent = "inner",
+					},
+					join = {
+						space_in_brackets = true,
+						separator = ", ",
+					},
+				},
+			},
+		},
 	},
 }
