93148bf72f9000192b11077d8e37ee88bc4a99a4
Diogo Pombeiro <diogopombeiro@Diogos-MacBook-Pro.local>
2025-10-10 18:03:17 +0200

    feat(nvim.colorizer): enable other types of color representation

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,12 +1,17 @@
 return {
-  "catgoose/nvim-colorizer.lua",
-  event = { "BufNewFile", "BufReadPost" },
-  opts = {
-    filetypes = {
-      "*",
-    },
-    user_default_options = {
-      always_update = true,
-    }
-  }
+	"catgoose/nvim-colorizer.lua",
+	event = { "BufNewFile", "BufReadPost" },
+	opts = {
+		user_default_options = {
+			RGB = true,
+			RRGGBB = true,
+			names = true,
+			RRGGBBAA = true,
+			rgb_fn = true,
+			hsl_fn = false,
+			css = true,
+			css_fn = true,
+			always_update = true,
+		},
+	},
 }
