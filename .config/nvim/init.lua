ee9047f5d1b64769a9195ea36706cce7fed1af28
Diogo Pombeiro <diogofpombeiro@gmail.com>
2025-10-20 23:47:12 +0500

    feat(nvim): implement review system using 'gh'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,6 +60,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.ui.smear-cursor" },
 		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
 		{ import = "lazyvim.plugins.extras.util.dot" },
+		{ import = "lazyvim.plugins.extras.util.gh" },
 		{ import = "lazyvim.plugins.extras.util.startuptime" },
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "plugins" },
