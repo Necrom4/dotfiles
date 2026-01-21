0b31766a9e7fce62f4b78d76ceeb799602c8e480
Diogo Pombeiro <dp6731@PF4NPQRM.infra.c2i.ch>
2026-01-21 14:20:59 +0100

    fix(yadm): ssh test

--- .config/yadm/bootstrap.d/00-set-yadm-remote.sh
+++ .config/yadm/bootstrap.d/00-set-yadm-remote.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env sh
 
-echo "Updating the yadm repo origin URL"
+# Default to HTTPS
 yadm remote set-url origin "https://github.com/Necrom4/dotfiles.git"
 
-ssh -o ConnectTimeout=3 -T git@github.com >/dev/null 2>&1
-ssh_status=$?
+echo "Testing SSH connectivity to GitHub..."
 
-if [ "$ssh_status" -eq 1 ] || [ "$ssh_status" -eq 0 ]; then
+if
+  ssh -o ConnectTimeout=3 -o BatchMode=yes -T git@github.com >/dev/null 2>&1
+  [ $? -eq 1 ] ||
+    ssh -o ConnectTimeout=3 -o BatchMode=yes -p 443 -T git@ssh.github.com >/dev/null 2>&1
+  [ $? -eq 1 ]
+then
+
+  echo "SSH connection successful. Setting SSH push URL."
   yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
 else
+  echo "SSH connection failed or timed out. Falling back to HTTPS for push."
   yadm remote set-url --push origin "https://github.com/Necrom4/dotfiles.git"
 fi
