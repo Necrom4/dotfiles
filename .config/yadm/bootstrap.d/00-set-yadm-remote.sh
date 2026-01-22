fe57c5275cfed8984e288240ad2bc3a741e0fe39
Diogo Pombeiro <dp6731@PF4NPQRM.infra.c2i.ch>
2026-01-22 09:57:17 +0100

    fix(yadm.bootstrap.set-yadm-remote): try simpler command, this doesn't set 443 if 22 doesn't work

--- .config/yadm/bootstrap.d/00-set-yadm-remote.sh
+++ .config/yadm/bootstrap.d/00-set-yadm-remote.sh
@@ -6,12 +6,11 @@ yadm remote set-url origin "https://github.com/Necrom4/dotfiles.git"
 echo "Testing SSH connectivity to GitHub..."
 
 if
-  ssh -o ConnectTimeout=3 -o BatchMode=yes -T git@github.com >/dev/null 2>&1
+  ssh -T git@github.com >/dev/null 2>&1
   [ $? -eq 1 ] ||
-    ssh -o ConnectTimeout=3 -o BatchMode=yes -p 443 -T git@ssh.github.com >/dev/null 2>&1
+    ssh -T -p 443 git@ssh.github.com >/dev/null 2>&1
   [ $? -eq 1 ]
 then
-
   echo "SSH connection successful. Setting SSH push URL."
   yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
 else
