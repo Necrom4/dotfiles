9566cad67188e2d51e45e81f64a3aa2239d9c26f
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-01-31 21:57:12 +0100

    fix(yadm): simplify way of setting repo remote
    Unable to push with `https` anyway, so force use `ssh`. Solves conditions not working

--- .config/yadm/bootstrap.d/00-set-yadm-remote.sh
+++ .config/yadm/bootstrap.d/00-set-yadm-remote.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env sh
 
 # Default to HTTPS
+echo "Setting the yadm repo origin URL"
 yadm remote set-url origin "https://github.com/Necrom4/dotfiles.git"
-
-echo "Testing SSH connectivity to GitHub..."
-
-if
-  ssh -T git@github.com >/dev/null 2>&1
-  [ $? -eq 1 ] ||
-    ssh -T -p 443 git@ssh.github.com >/dev/null 2>&1
-  [ $? -eq 1 ]
-then
-  echo "SSH connection successful. Setting SSH push URL."
-  yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
-else
-  echo "SSH connection failed or timed out. Falling back to HTTPS for push."
-  yadm remote set-url --push origin "https://github.com/Necrom4/dotfiles.git"
-fi
+yadm remote set-url --push origin "git@github.com:Necrom4/dotfiles.git"
