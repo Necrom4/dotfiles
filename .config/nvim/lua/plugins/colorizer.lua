8aa411acbc9708f6157a5c25399906c3ae943968
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-02-11 19:28:37 +0100

    fix(neovim): use `nvim-colorizer` only
    Ditch mini-hipatterns

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,17 +1,33 @@
+local colored = true
+
 return {
 	"catgoose/nvim-colorizer.lua",
 	event = { "BufNewFile", "BufReadPost" },
-	opts = {
-		user_default_options = {
-			RGB = true,
-			RRGGBB = true,
-			names = true,
-			RRGGBBAA = true,
-			rgb_fn = true,
-			hsl_fn = false,
-			css = true,
-			css_fn = true,
-			always_update = true,
-		},
-	},
+	opts = function()
+		Snacks.toggle({
+			id = "colorizer",
+			name = "Colorizer",
+			get = function()
+				return colored
+			end,
+			set = function()
+				colored = not colored
+				vim.cmd("ColorizerToggle")
+			end,
+		}):map("<leader>uC")
+
+		return {
+			user_default_options = {
+				RGB = true,
+				RRGGBB = true,
+				names = true,
+				RRGGBBAA = true,
+				rgb_fn = true,
+				hsl_fn = false,
+				css = true,
+				css_fn = true,
+				always_update = true,
+			},
+		}
+	end,
 }
