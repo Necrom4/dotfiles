c444bb64d6cea20388a838862ad85435880bb889
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-02-13 16:59:54 +0100

    feat(neovim): enable sandman after fixes were pushed

--- .config/nvim/lua/plugins/sandman.lua
+++ .config/nvim/lua/plugins/sandman.lua
@@ -1,24 +1,25 @@
 return {
 	"stasfilin/nvim-sandman",
-	-- disabled for now, sometimes blocks allowed plugins
-	enabled = false,
 	opts = {
 		enabled = true,
 		mode = "block_all",
+		env_block = false,
 		allow = {
 			"blink-cmp-git",
-			"blink.cmp",
 			"camouflage.nvim",
-			"gitsigns.nvim",
+			"conform.nvim",
 			"lazy.nvim",
 			"mason-lspconfig.nvim",
 			"mason.nvim",
 			"snacks.nvim",
 			"thanks.nvim",
-			-- to block whenever feature to ignore their notifications is available
-			"conform.nvim",
-			"haunt.nvim",
 			"plenary.nvim",
+		},
+		ignore_notifications = {
+			"gitsigns.nvim",
+			"haunt.nvim",
+			"mason-lspconfig.nvim",
+			"mason.nvim",
 		},
 	},
 }
