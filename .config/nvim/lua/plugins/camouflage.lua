1d5609dc5271377e21cfec61fddc74f296f291e4
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-02-19 10:35:43 +0100

    feat(neovim.camouflage): use Snacks.toggle keymaps

--- .config/nvim/lua/plugins/camouflage.lua
+++ .config/nvim/lua/plugins/camouflage.lua
@@ -1,21 +1,69 @@
+local concealed = true
+local concealed_line = true
+
 return {
 	"zeybek/camouflage.nvim",
 	event = "VeryLazy",
-	opts = {
-		pwned = {
-			enabled = false,
-			sign_text = "",
-		},
-	},
+	opts = function()
+		Snacks.toggle({
+			id = "comouflage_toggle",
+			name = "values",
+			get = function()
+				return concealed
+			end,
+			set = function()
+				concealed = not concealed
+				vim.cmd("CamouflageToggle")
+			end,
+			icon = {
+				enabled = " ",
+				disabled = " ",
+			},
+			color = {
+				enabled = "red",
+				disabled = "azure",
+			},
+			wk_desc = {
+				enabled = "Show ",
+				disabled = "Conceal ",
+			},
+		}):map("<leader>ucC")
+
+		Snacks.toggle({
+			id = "comouflage_follow_cursor",
+			name = "cursor line",
+			get = function()
+				return concealed_line
+			end,
+			set = function()
+				concealed_line = not concealed_line
+				vim.cmd("CamouflageFollowCursor")
+			end,
+			icon = {
+				enabled = " ",
+				disabled = " ",
+			},
+			color = {
+				enabled = "red",
+				disabled = "azure",
+			},
+			wk_desc = {
+				enabled = "Show ",
+				disabled = "Conceal ",
+			},
+		}):map("<leader>ucc")
+
+		return {
+			pwned = {
+				enabled = false,
+				sign_text = "",
+			},
+		}
+	end,
 	keys = {
-		{ "<leader>uct", "<cmd>CamouflageToggle<cr>", desc = "Toggle Camouflage" },
-		{ "<leader>ucr", "<cmd>CamouflageReveal<cr>", desc = "Reveal Line" },
-		{ "<leader>ucy", "<cmd>CamouflageYank<cr>", desc = "Yank Value" },
-		{ "<leader>ucf", "<cmd>CamouflageFollowCursor<cr>", desc = "Follow Cursor" },
 		{
 			"<leader>cP",
 			function()
-				-- vim.cmd("Sandman temp-net 1000")
 				vim.cmd("CamouflagePwnedCheckLine")
 			end,
 			desc = "Pwned?",
