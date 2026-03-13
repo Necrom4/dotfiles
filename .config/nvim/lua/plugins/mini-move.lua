69974b5bb2a7bee834feee2a0685a1ec3edcb7c2
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-03-13 13:19:21 +0100

    feat(neovim/tmux): integrate tmux with nvim
    - tmux/nvim window navigation, adds new `<M-hjkl>` keymaps
    - restore nvim sessions
    
    Had to only leave mini.move enabled for visual mode

---
+++ .config/nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-mini/mini.move",
+	opts = {
+		mappings = {
+			line_left = "",
+			line_down = "",
+			line_up = "",
+			line_right = "",
+		},
+	},
+}
---
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,21 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+	keys = {
+		{ "<M-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left (tmux/vim)", mode = { "n", "t" } },
+		{ "<M-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down (tmux/vim)", mode = { "n", "t" } },
+		{ "<M-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up (tmux/vim)", mode = { "n", "t" } },
+		{ "<M-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right (tmux/vim)", mode = { "n", "t" } },
+		{ "<M-w>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Navigate to previous (tmux/vim)", mode = { "n", "t" } },
+	},
+}
