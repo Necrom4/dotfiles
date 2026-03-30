4bebea3dcde0517d79f96d9ed53cf474bd5a7a58
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-03-30 11:31:23 +0200

    fix(neovim.smear-cursor): improve smootheness
    - Increase frame rates
    - Remove smear
    - disable hide_target_hack (seemed to duplicate cursor)

---
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		legacy_computing_symbols_support = true,
+		stiffness = 0.5,
+		trailing_stiffness = 0.5,
+		matrix_pixel_threshold = 0.5,
+		time_interval = 7,
+		hide_target_hack = false,
+	},
+}
