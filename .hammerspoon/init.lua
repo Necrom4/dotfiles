cb5e0da1ba2668dc706707cc1318f67bb3d84406
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-04-12 23:52:32 +0200

    feat(hammerspoon): automate modules loading

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,18 +1,41 @@
+-- Alerts
+local function alertStyle(color)
+	return {
+		fillColor = hs.drawing.color.asRGB({ alpha = 0.75 }),
+		strokeColor = color,
+		radius = 10,
+	}
+end
+
+local styleOk = alertStyle(hs.drawing.color.asRGB({ red = 0.9, green = 0.75, blue = 0.0, alpha = 0.75 }))
+local styleErr = alertStyle(hs.drawing.color.asRGB({ red = 0.9, green = 0.2, blue = 0.2, alpha = 0.75 }))
+
+-- Auto-reload config
 local function reloadConfig(files)
-	local shouldReload = false
+	local doReload = false
 	for _, file in pairs(files) do
 		if file:sub(-4) == ".lua" then
-			shouldReload = true
+			doReload = true
 		end
 	end
-	if shouldReload then
+	if doReload then
 		hs.reload()
 	end
 end
 
 local watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
 watcher:start()
-hs.alert.show("Hammerspoon reloaded")
+hs.alert.show("Config reloaded", styleOk)
 
 -- Load modules
-require("modules.clipboard")
+local modules = {
+	"clipboard",
+}
+
+for _, name in ipairs(modules) do
+	local ok, err = pcall(require, "modules." .. name)
+	if not ok then
+		hs.alert("Error in [" .. name .. "]", styleErr)
+		print("Error loading module: " .. name .. "\n" .. err)
+	end
+end
