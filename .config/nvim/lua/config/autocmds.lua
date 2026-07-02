4b01241cb6c6aa0c887b2b936a3ea212145ee7aa
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-07-02 18:20:51 +0200

    chore(neovim): remove sqlite table display autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -20,38 +20,3 @@ vim.api.nvim_create_autocmd("User", {
 		}
 	end,
 })
-
--- Display table instead of binary in .sqlite files
-vim.api.nvim_create_autocmd("BufReadPre", {
-	pattern = "*.sqlite",
-	callback = function()
-		local filepath = vim.fn.expand("%:p")
-
-		-- Get first table name
-		local table_name = utils.term_cmd('sqlite3 "' .. filepath .. '" ".tables"'):match("(%S+)")
-		if not table_name then
-			vim.notify("No tables found in database.", vim.log.levels.WARN)
-			return
-		end
-
-		-- Build SQL query
-		local query = 'sqlite3 -header -column "' .. filepath .. '" "SELECT * FROM ' .. table_name .. ';"'
-
-		-- Replace current buffer content with query result
-		vim.schedule(function()
-			vim.cmd("%delete _") -- delete all lines quietly
-			local data = vim.fn.systemlist(query)
-			if vim.v.shell_error ~= 0 or not data or #data == 0 then
-				data = { "Error querying database." }
-			end
-
-			vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
-			vim.bo.buftype = "nofile"
-			vim.bo.bufhidden = "wipe"
-			vim.bo.swapfile = false
-			vim.bo.modifiable = false
-			vim.bo.readonly = true
-			vim.bo.filetype = "sql"
-		end)
-	end,
-})
