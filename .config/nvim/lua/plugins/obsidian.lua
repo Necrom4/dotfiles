1a7f8228bf2b528753db5ac449626a794e61e7d5
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-04 16:13:16 +0200

    chore(neovim.obsidian): set workdays_only to true

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -18,9 +18,6 @@ return {
 	version = "*", -- use latest release, remove to use latest commit
 	opts = {
 		legacy_commands = false, -- this will be removed in 4.0.0
-		daily_notes = {
-			workdays_only = false,
-		},
 		ui = {
 			enable = false,
 		},
