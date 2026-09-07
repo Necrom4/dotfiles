7c7615b3d66f56be9abbfa8c35a2fcab88ea132a
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-07 12:55:47 +0200

    style(neovim.bullets): change signs

--- .config/nvim/lua/plugins/bullets.lua
+++ .config/nvim/lua/plugins/bullets.lua
@@ -3,7 +3,7 @@ return {
 	ft = { "markdown", "text", "gitcommit", "scratch" },
 	opts = {
 		enabled_file_types = { "markdown", "text", "gitcommit", "scratch" },
-		checkbox_markers = " .oOx",
+		checkbox_markers = " 󰪞󰪟󰪠󰪡󰪢󰪣󰪤x",
 	},
 	keys = {
 		{
