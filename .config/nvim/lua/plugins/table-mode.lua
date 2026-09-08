e41081271645c3d136b4e935da882e0df976c942
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-08 10:26:27 +0200

    fix(neovim.table_mode): remove annoying TableModeEnable message

--- .config/nvim/lua/plugins/table-mode.lua
+++ .config/nvim/lua/plugins/table-mode.lua
@@ -7,7 +7,7 @@ return {
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "markdown",
 			callback = function()
-				vim.cmd("TableModeEnable")
+				vim.cmd("silent! TableModeEnable")
 			end,
 		})
 	end,
