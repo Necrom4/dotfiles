92a28f2c562ab85813255c1ae805a252ff43eaf5
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-08 10:07:13 +0200

    perf(neovim.obsidian): put in function obsidian
    Allows for better start up performance.
    Also fix problem that would block loading of all plugins if vault didn't
    exist.

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,31 +1,40 @@
-local utils = require("utils.general")
-
-local class = utils.yadm_config("local.class")
-
-class = vim.trim(string.lower(class))
-assert(class ~= "", "yadm config local.class is not set")
-
-local vault_path = vim.fn.expand("~/vaults/" .. class)
-
-if vim.fn.isdirectory(vault_path) == 0 then
-	vim.fn.mkdir(vault_path, "p")
-end
-
 return {
 	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- use latest release, remove to use latest commit
-	opts = {
-		legacy_commands = false, -- this will be removed in 4.0.0
-		ui = {
-			enable = false,
-		},
-		workspaces = {
-			{
-				name = class,
-				path = vault_path,
+	-- Everything below is deferred into `opts`. Previously the yadm `git config`
+	-- lookup and the mkdir ran at spec-parse time, i.e. a blocking subprocess on
+	-- every single startup, and the `assert` meant an unset `local.class` took
+	-- down the whole `{ import = "plugins" }` module rather than just this plugin.
+	opts = function()
+		local utils = require("utils.general")
+
+		local class = vim.trim(string.lower(utils.yadm_config("local.class")))
+		if class == "" then
+			vim.notify(
+				"obsidian.nvim: yadm config `local.class` is not set; falling back to 'default'",
+				vim.log.levels.WARN
+			)
+			class = "default"
+		end
+
+		local vault_path = vim.fn.expand("~/vaults/" .. class)
+		if vim.fn.isdirectory(vault_path) == 0 then
+			vim.fn.mkdir(vault_path, "p")
+		end
+
+		return {
+			legacy_commands = false, -- this will be removed in 4.0.0
+			ui = {
+				enable = false,
 			},
-		},
-	},
+			workspaces = {
+				{
+					name = class,
+					path = vault_path,
+				},
+			},
+		}
+	end,
 	cmd = { "Obsidian" },
 	keys = {
 		{ "<leader>fo", "<cmd>Obsidian<cr>", desc = "Obsidian" },
