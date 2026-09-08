e0f9069bf7dfa9146030194963286cfe77ac161b
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-08 10:09:35 +0200

    style: use `"` instead of `'`

--- .config/nvim/lua/plugins/scrollbar.lua
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -1,26 +1,26 @@
 return {
-  "petertriho/nvim-scrollbar",
-  dependencies = { "kevinhwang91/nvim-hlslens", },
-  event = { "BufNewFile", "BufReadPost" },
-  opts = {
-    handle = {
-      highlight = 'Cursor',
-    },
-    marks = {
-      Cursor = { text = '▒' },
-      Search = { text = { '', '󰇙' }, highlight = 'ScrollBarSearch' },
-      Error = { text = { '━', '═' }, highlight = 'ScrollBarError' },
-      Warn = { text = { '━', '═' }, highlight = 'ScrollBarWarn' },
-      Info = { text = { '━', '═' }, highlight = 'ScrollBarInfo' },
-      Hint = { text = { '━', '═' }, highlight = 'ScrollBarHint' },
-      Misc = { text = { '━', '═' }, highlight = 'ScrollBarMisc' },
-      GitAdd = { text = '┃', highlight = 'ScrollbarGitAdd' },
-      GitChange = { text = '┃', highlight = 'ScrollbarGitChange' },
-      GitDelete = { highlight = 'ScrollbarGitDelete' },
-    },
-    handlers = { search = true },
-    excluded_buftypes = {
-      "nofile",
-    },
-  }
+	"petertriho/nvim-scrollbar",
+	dependencies = { "kevinhwang91/nvim-hlslens" },
+	event = { "BufNewFile", "BufReadPost" },
+	opts = {
+		handle = {
+			highlight = "Cursor",
+		},
+		marks = {
+			Cursor = { text = "▒" },
+			Search = { text = { "", "󰇙" }, highlight = "ScrollBarSearch" },
+			Error = { text = { "━", "═" }, highlight = "ScrollBarError" },
+			Warn = { text = { "━", "═" }, highlight = "ScrollBarWarn" },
+			Info = { text = { "━", "═" }, highlight = "ScrollBarInfo" },
+			Hint = { text = { "━", "═" }, highlight = "ScrollBarHint" },
+			Misc = { text = { "━", "═" }, highlight = "ScrollBarMisc" },
+			GitAdd = { text = "┃", highlight = "ScrollbarGitAdd" },
+			GitChange = { text = "┃", highlight = "ScrollbarGitChange" },
+			GitDelete = { highlight = "ScrollbarGitDelete" },
+		},
+		handlers = { search = true },
+		excluded_buftypes = {
+			"nofile",
+		},
+	},
 }
