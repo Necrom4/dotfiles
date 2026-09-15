27524dc79e442c9058e90d0dba92fd653ea31e39
Diogo Pombeiro <diogo.ferreirapombeiro@ruag.ch>
2026-09-15 11:25:46 +0200

    feat(neovim.atlas): add reviewing gitlab view
    Other two are already there by default

--- .config/nvim/lua/plugins/atlas.lua
+++ .config/nvim/lua/plugins/atlas.lua
@@ -12,5 +12,26 @@ return {
 				token = vim.env.GITLAB_TOKEN,
 			},
 		},
+		pulls = {
+			gitlab = {
+				views = {
+					{
+						name = "Reviewing",
+						key = "1",
+						scope = "reviews_for_me",
+					},
+					{
+						name = "Assigned",
+						key = "2",
+						scope = "assigned_to_me",
+					},
+					{
+						name = "Created",
+						key = "3",
+						scope = "created_by_me",
+					},
+				},
+			},
+		},
 	},
 }
