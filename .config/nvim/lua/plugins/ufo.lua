7dd1bc0e8f2b67174b76ca7d9413e88472b667a3
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-09-24 22:45:03 +0200

    fix(neovim.ufo): stop LazyVim's LSP/treesitter folds competing with ufo
    LazyVim still set foldmethod=expr with its own foldexpr on LspAttach and
    FileType, racing ufo's manual folds.

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -39,72 +39,77 @@ local ftMap = {
 }
 
 return {
-	"kevinhwang91/nvim-ufo",
-	dependencies = {
-		"kevinhwang91/promise-async",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	event = { "BufReadPost", "BufNewFile" },
-	init = function()
-		vim.opt.foldcolumn = "1"
-		vim.opt.foldlevelstart = 99
-		vim.opt.foldnestmax = 6
-		vim.opt.foldenable = true
-		vim.opt.foldopen = "block,insert,jump,mark,percent,quickfix,search,tag,undo"
-	end,
-	keys = {
-		{
-			"zR",
-			function()
-				require("ufo").openAllFolds()
-			end,
-			mode = { "n", "v" },
-			desc = "Open All Folds",
+	-- ufo owns folding, so stop LazyVim from also wiring LSP/treesitter foldexpr
+	{ "neovim/nvim-lspconfig", opts = { folds = { enabled = false } } },
+	{ "nvim-treesitter/nvim-treesitter", opts = { folds = { enable = false } } },
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"nvim-treesitter/nvim-treesitter",
 		},
-		{
-			"zM",
-			function()
-				require("ufo").closeAllFolds()
-			end,
-			mode = { "n", "v" },
-			desc = "Close All Folds",
-		},
-		{
-			"zp",
-			function()
-				local winid = require("ufo").peekFoldedLinesUnderCursor()
-				if not winid then
-					vim.lsp.buf.hover()
-				end
-			end,
-			desc = "Peek folded lines",
-			mode = { "n", "v" },
-		},
-	},
-	opts = {
-		open_fold_hl_timeout = 150,
-		enable_get_fold_virt_text = true,
-		fold_virt_text_handler = Fold.ufo_virt_text_handler_enhanced,
-		close_fold_kinds_for_ft = {
-			default = { "imports", "comment" },
-			json = { "array" },
-			jsonc = { "array" },
-			c = { "comment", "region" },
-			cpp = { "comment", "region" },
-			java = { "comment", "imports" },
-			javascript = { "comment", "imports" },
-			typescript = { "comment", "imports" },
-			vue = { "imports" },
-			python = { "comment", "imports" },
-			go = { "comment", "imports" },
-			rust = { "comment", "imports" },
-			php = { "imports" },
-		},
-		provider_selector = function(bufnr, filetype, buftype)
-			if buftype ~= "" then
-				return nil
-			end
-			return ftMap[filetype] or Fold.ufo_provider_selector
+		event = { "BufReadPost", "BufNewFile" },
+		init = function()
+			vim.opt.foldcolumn = "1"
+			vim.opt.foldlevelstart = 99
+			vim.opt.foldnestmax = 6
+			vim.opt.foldenable = true
+			vim.opt.foldopen = "block,insert,jump,mark,percent,quickfix,search,tag,undo"
 		end,
+		keys = {
+			{
+				"zR",
+				function()
+					require("ufo").openAllFolds()
+				end,
+				mode = { "n", "v" },
+				desc = "Open All Folds",
+			},
+			{
+				"zM",
+				function()
+					require("ufo").closeAllFolds()
+				end,
+				mode = { "n", "v" },
+				desc = "Close All Folds",
+			},
+			{
+				"zp",
+				function()
+					local winid = require("ufo").peekFoldedLinesUnderCursor()
+					if not winid then
+						vim.lsp.buf.hover()
+					end
+				end,
+				desc = "Peek folded lines",
+				mode = { "n", "v" },
+			},
+		},
+		opts = {
+			open_fold_hl_timeout = 150,
+			enable_get_fold_virt_text = true,
+			fold_virt_text_handler = Fold.ufo_virt_text_handler_enhanced,
+			close_fold_kinds_for_ft = {
+				default = { "imports", "comment" },
+				json = { "array" },
+				jsonc = { "array" },
+				c = { "comment", "region" },
+				cpp = { "comment", "region" },
+				java = { "comment", "imports" },
+				javascript = { "comment", "imports" },
+				typescript = { "comment", "imports" },
+				vue = { "imports" },
+				python = { "comment", "imports" },
+				go = { "comment", "imports" },
+				rust = { "comment", "imports" },
+				php = { "imports" },
+			},
+			provider_selector = function(bufnr, filetype, buftype)
+				if buftype ~= "" then
+					return nil
+				end
+				return ftMap[filetype] or Fold.ufo_provider_selector
+			end,
+		},
 	},
 }
