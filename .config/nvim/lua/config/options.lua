1f2d6c58c1c35e080e31892d033ce6e72f26dfc6
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-09-26 11:30:31 +0200

    chore(neovim): drop redundant hidden option

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,4 @@
 vim.opt.autowrite = false -- Enable auto write
-vim.opt.hidden = true -- This makes vim act like all other editors, buffers can exist in the background without being in a window.
 vim.opt.list = false -- Show some invisible characters (tabs...
 vim.opt.scrolloff = 0 -- Lines of context
 vim.opt.wrap = false -- Disable line wrap
