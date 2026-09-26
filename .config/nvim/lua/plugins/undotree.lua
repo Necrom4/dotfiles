30bcf36973ef574d8d12526dfd640dd96b7f2b7d
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-09-26 12:52:16 +0200

    feat(neovim): replace mbbill undotree with Atone
    Use native Atone toggle with auto-attach, diff preview and persistent marks.

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -1,39 +1,12 @@
-local undotree_enabled = false
-
 return {
-	"mbbill/undotree",
-	config = function()
-		vim.g.undotree_WindowLayout = 2
-		vim.g.undotree_ShortIndicators = 1
-		vim.g.undotree_TreeNodeShape = ""
-		vim.g.undotree_TreeVertShape = "│"
-	end,
-	opts = function()
-		Snacks.toggle
-			.new({
-				id = "undotree",
-				name = "Undotree",
-				get = function()
-					return undotree_enabled
-				end,
-				set = function()
-					undotree_enabled = not undotree_enabled
-					vim.cmd("UndotreeToggle")
-				end,
-				icon = {
-					enabled = "󰕌 ",
-					disabled = "󰕌 ",
-				},
-				color = {
-					enabled = "red",
-					disabled = "purple",
-				},
-				wk_desc = {
-					enabled = "Close ",
-					disabled = "Open ",
-				},
-			})
-			:map("<leader>du")
-		return {}
-	end,
+	"XXiaoA/atone.nvim",
+	cmd = "Atone",
+	keys = {
+		{ "<leader>du", "<cmd>Atone toggle<cr>", desc = "Toggle undo tree" },
+	},
+	opts = {
+		auto_attach = { enabled = true },
+		diff_cur_node = { enabled = true },
+		marks = { persist = true },
+	},
 }
