b2705470484c5054550c4c4eba3b15de43ca6ae0
Diogo Pombeiro <diogofpombeiro@gmail.com>
2026-09-26 12:46:45 +0200

    fix(neovim): keep bufferline tab-only and hide inapplicable buffer actions

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,14 @@
 return {
 	"akinsho/bufferline.nvim",
-	event = { "BufWinEnter" },
+	event = "VeryLazy",
+	keys = {
+		-- LazyVim's buffer-only actions are misleading in tabpage mode.
+		{ "<leader>bp", false },
+		{ "<leader>bP", false },
+		{ "<leader>br", false },
+		{ "<leader>bl", false },
+		{ "<leader>bj", false },
+	},
 	opts = {
 		options = {
 			mode = "tabs",
